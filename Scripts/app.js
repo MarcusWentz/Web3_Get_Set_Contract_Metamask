ec56a25c14dfb2cb9b918d50a06d99483102fb69
MarcusWentz <52706599+MarcusWentz@users.noreply.github.com>
2021-07-19 21:26:52 -0400

    Delete Scripts directory

--- Scripts/app.js
+++
@@ -1,6 +0,0 @@
-//Define button click for opening Metamask
-const ethereumButton = document.querySelector('.enableEthereumButton');
-ethereumButton.addEventListener('click', () => {
-  //Will Start the metamask extension
-  ethereum.request({ method: 'eth_requestAccounts' });
-});
