34d27e4d325af7ddcdbd58cc5515f92888bf2012
MarcusWentz <52706599+MarcusWentz@users.noreply.github.com>
2021-07-22 18:31:17 -0400

    Update app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -61,6 +61,7 @@ try{
 const web3 = new Web3(window.ethereum)
 }
 catch(error){
+  console.log(error)
   document.getElementById("getValueStateSmartContract").innerHTML =  "INSTALL METAMASK!"
 }
 finally{
