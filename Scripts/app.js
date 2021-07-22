203ac310512f900e70f73d6a98df4a9d7f39d252
MarcusWentz <52706599+MarcusWentz@users.noreply.github.com>
2021-07-22 18:45:59 -0400

    Update app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -58,20 +58,22 @@ changeStateInContractEvent.addEventListener('click', () => {
 //TESTING WEB3 FOR CALLING INFO /////////////////////////////////////////////////
 //import * as Web3 from 'web3';
 try{
-  alert(ethereum.isMetaMask)
+  ethereum.isMetaMask
 }
 catch(error){
   alert(error)
   document.getElementById("getValueStateSmartContract").innerHTML =  "INSTALL METAMASK!"
 }
-finally{
-  const web3 = new Web3(window.ethereum)
-  const contractAddress_JS = '0x6B6a427CaCc6adB23117ff4EFef5e6365617bA94'
-  const contractABI_JS = //[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"get","outputs":[{"internalType":"uint256","name":"retVal","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
-  [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"date","type":"uint256"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":false,"internalType":"uint256","name":"valueChangeEventWenjs","type":"uint256"}],"name":"setValueUpdatedViaWebjs","type":"event"},{"inputs":[],"name":"get","outputs":[{"internalType":"uint256","name":"retVal","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
-  const contractDefined_JS = new web3.eth.Contract(contractABI_JS, contractAddress_JS)
 
-    contractDefined_JS.methods.get().call((err, balance) => {
-    document.getElementById("getValueStateSmartContract").innerHTML =  balance
-    }
-}
+// finally{
+// }
+
+const web3 = new Web3(window.ethereum)
+const contractAddress_JS = '0x6B6a427CaCc6adB23117ff4EFef5e6365617bA94'
+const contractABI_JS = //[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"get","outputs":[{"internalType":"uint256","name":"retVal","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
+[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"date","type":"uint256"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":false,"internalType":"uint256","name":"valueChangeEventWenjs","type":"uint256"}],"name":"setValueUpdatedViaWebjs","type":"event"},{"inputs":[],"name":"get","outputs":[{"internalType":"uint256","name":"retVal","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
+const contractDefined_JS = new web3.eth.Contract(contractABI_JS, contractAddress_JS)
+
+  contractDefined_JS.methods.get().call((err, balance) => {
+  document.getElementById("getValueStateSmartContract").innerHTML =  balance
+  }
