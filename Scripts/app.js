8aa6a6339916957b31d9dfd56a58a25c24f07903
MarcusWentz <52706599+MarcusWentz@users.noreply.github.com>
2021-07-22 18:51:22 -0400

    Update app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -75,6 +75,6 @@ const contractABI_JS = //[{"inputs":[],"stateMutability":"nonpayable","type":"co
 [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"date","type":"uint256"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":false,"internalType":"uint256","name":"valueChangeEventWenjs","type":"uint256"}],"name":"setValueUpdatedViaWebjs","type":"event"},{"inputs":[],"name":"get","outputs":[{"internalType":"uint256","name":"retVal","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
 const contractDefined_JS = new web3.eth.Contract(contractABI_JS, contractAddress_JS)
 
-  contractDefined_JS.methods.get().call((err, balance) => {
+contractDefined_JS.methods.get().call((err, balance) => {
   document.getElementById("getValueStateSmartContract").innerHTML =  balance
 })
