8958a0ba58ee5aaba8d81e3d6bfac143106a518c
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-07-25 23:14:26 -0400

    Update readStateAndEvent.go

--- Scripts/readStateAndEvent.go
+++ Scripts/readStateAndEvent.go
@@ -5,10 +5,14 @@ import (
     "fmt"
     "log"
     "os"
+    "context"
 
+    "github.com/ethereum/go-ethereum"
     "github.com/ethereum/go-ethereum/accounts/abi/bind"
     "github.com/ethereum/go-ethereum/common"
     "github.com/ethereum/go-ethereum/ethclient"
+    "github.com/ethereum/go-ethereum/core/types"
+
 )
 
 func main() {
@@ -17,18 +21,44 @@ func main() {
         log.Fatal(err)
     }
 
-    contract, err := NewStore(common.HexToAddress("0xaf3310ec212ecba069149239f954f1281fda836b"), client)
-    if err != nil {
-        log.Fatal(err)
-    }
+    contractAddress := common.HexToAddress("0xaf3310ec212eCBA069149239F954F1281fDa836B")
+     query := ethereum.FilterQuery{
+         Addresses: []common.Address{contractAddress},
+     }
 
-    fmt.Println("contract is loaded")
-    _ = contract
+     logs := make(chan types.Log)
+     sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
+     if err != nil {
+         log.Fatal(err)
+     }
 
-    storedData, err := contract.StoredData(&bind.CallOpts{})
-      if err != nil {
-          log.Fatal(err)
-    }
+     contract, err := NewStore(contractAddress, client)
+     if err != nil {
+         log.Fatal(err)
+     }
 
-    fmt.Println(storedData)
+     fmt.Println("contract is loaded")
+     _ = contract
+
+     storedData, err := contract.StoredData(&bind.CallOpts{})
+       if err != nil {
+           log.Fatal(err)
+     }
+
+     fmt.Println(storedData)
+
+     for {
+         select {
+         case err := <-sub.Err():
+             log.Fatal(err)
+         case vLog := <-logs:
+             fmt.Println(vLog) // pointer to event log
+             storedData, err := contract.StoredData(&bind.CallOpts{})
+               if err != nil {
+                   log.Fatal(err)
+             }
+
+             fmt.Println(storedData)
+         }
+     }
 }
