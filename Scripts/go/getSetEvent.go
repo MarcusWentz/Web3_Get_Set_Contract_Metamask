3a6e57393ca14ee2c095437e1668f04b8f703354
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-07-26 11:16:27 -0400

    Update and rename readStateAndEvent.go to getSetEvent.go

--- Scripts/go/getSetEvent.go
+++ Scripts/go/getSetEvent.go
@@ -6,13 +6,15 @@ import (
     "log"
     "os"
     "context"
+    "crypto/ecdsa"
+    "math/big"
 
     "github.com/ethereum/go-ethereum"
     "github.com/ethereum/go-ethereum/accounts/abi/bind"
     "github.com/ethereum/go-ethereum/common"
     "github.com/ethereum/go-ethereum/ethclient"
     "github.com/ethereum/go-ethereum/core/types"
-
+    "github.com/ethereum/go-ethereum/crypto"
 )
 
 func main() {
@@ -38,6 +40,46 @@ func main() {
      }
 
      fmt.Println(storedData)
+
+     //Set new value for smart contract uint storage slot.
+     privateKey, err := crypto.HexToECDSA(os.Getenv("devTestnetPrivateKey"))
+      if err != nil {
+          log.Fatal(err)
+      }
+
+      publicKey := privateKey.Public()
+      fmt.Println(publicKey)
+      publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+      if !ok {
+          log.Fatal("error casting public key to ECDSA")
+      }
+
+      fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+      nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+      if err != nil {
+          log.Fatal(err)
+      }
+
+      gasPrice, err := client.SuggestGasPrice(context.Background())
+      if err != nil {
+          log.Fatal(err)
+      }
+
+      fmt.Println(nonce)
+      fmt.Println(gasPrice)
+
+      auth := bind.NewKeyedTransactor(privateKey)
+      auth.Nonce = big.NewInt(int64(nonce))
+      auth.Value = big.NewInt(0)     // in wei
+      auth.GasLimit = uint64(300000) // in units
+      auth.GasPrice = gasPrice
+
+      tx, err := contract.Set(auth, big.NewInt(555))
+      if err != nil {
+          log.Fatal(err)
+      }
+
+      fmt.Printf("tx sent: %s", tx.Hash().Hex()) // tx sent
 
      //Subscribe to events from smart contract address.
      query := ethereum.FilterQuery{
