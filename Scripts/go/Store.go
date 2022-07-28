3224a3c7feba573bb246bba890f68a6b7ce5418b
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-07-28 18:28:33 -0400

    Update Store.go

--- Scripts/go/Store.go
+++ Scripts/go/Store.go
@@ -1,387 +1,115 @@
-// Code generated - DO NOT EDIT.
-// This file is a generated binding and any manual changes will be lost.
-
+//Step 1: Create abi file by running: solc --abi Store.sol > Store_sol_Store.abi
+//Step 2: Create bin file by running: solc --bin Store.sol > Store_sol_Store.bin
+//Step 3: Remove comments above the abi and bin files.
+//Step 4: Generate Go contract interaction file by running:  abigen --bin=Store_sol_Store.bin --abi=Store_sol_Store.abi --pkg=main --out=Store.g
+//Step 5: Run: StoreTest.go Store.go
 package main
 
 import (
-	"errors"
-	"math/big"
-	"strings"
+    "fmt"
+    "log"
+    "os"
+    "context"
+    "crypto/ecdsa"
+    "math/big"
 
-	ethereum "github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/event"
+    "github.com/ethereum/go-ethereum"
+    "github.com/ethereum/go-ethereum/accounts/abi/bind"
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/ethereum/go-ethereum/ethclient"
+    "github.com/ethereum/go-ethereum/core/types"
+    "github.com/ethereum/go-ethereum/crypto"
 )
 
-// Reference imports to suppress errors if they are not otherwise used.
-var (
-	_ = errors.New
-	_ = big.NewInt
-	_ = strings.NewReader
-	_ = ethereum.NotFound
-	_ = bind.Bind
-	_ = common.Big1
-	_ = types.BloomLookup
-	_ = event.NewSubscription
-)
+func main() {
+    //Get smart contract starting state.
+    client, err := ethclient.Dial("wss://ws.rpc.l16.lukso.network")
+    if err != nil {
+        log.Fatal(err)
+    }
 
-// MainMetaData contains all meta data concerning the Main contract.
-var MainMetaData = &bind.MetaData{
-	ABI: "[{\"anonymous\":false,\"inputs\":[],\"name\":\"setEvent\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"x\",\"type\":\"uint256\"}],\"name\":\"set\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"storedData\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]",
-	Bin: "0x608060405234801561001057600080fd5b50610179806100206000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c80632a1afcd91461003b57806360fe47b114610059575b600080fd5b610043610075565b60405161005091906100ca565b60405180910390f35b610073600480360381019061006e9190610116565b61007b565b005b60005481565b806000819055507f24a4f809cc1bf5b0f3c16b615535b56a0b583b1630e06e32b46b759b8088a95d60405160405180910390a150565b6000819050919050565b6100c4816100b1565b82525050565b60006020820190506100df60008301846100bb565b92915050565b600080fd5b6100f3816100b1565b81146100fe57600080fd5b50565b600081359050610110816100ea565b92915050565b60006020828403121561012c5761012b6100e5565b5b600061013a84828501610101565b9150509291505056fea26469706673582212200da6f6eb2a25a6a0fd086c77d3b6178d214c0f751d04d94854b7e5f38a20760864736f6c634300080f0033",
-}
+    chainID, err := client.NetworkID(context.Background())
+     if err != nil {
+         log.Fatal(err)
+     }
+     fmt.Println("Chain id: ", chainID)
 
-// MainABI is the input ABI used to generate the binding from.
-// Deprecated: Use MainMetaData.ABI instead.
-var MainABI = MainMetaData.ABI
+     contractAddress := common.HexToAddress("0x50684c64F4b80b5687d0891c9339De8fFE281A33")
+     contract, err := NewMain(contractAddress, client)
+     if err != nil {
+         log.Fatal(err)
+     }
+     fmt.Println("Contract is loaded at address", contractAddress)
 
-// MainBin is the compiled bytecode used for deploying new contracts.
-// Deprecated: Use MainMetaData.Bin instead.
-var MainBin = MainMetaData.Bin
+     storedData, err := contract.StoredData(&bind.CallOpts{})
+       if err != nil {
+           log.Fatal(err)
+     }
+     fmt.Println("storedData:", storedData)
 
-// DeployMain deploys a new Ethereum contract, binding an instance of Main to it.
-func DeployMain(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Main, error) {
-	parsed, err := MainMetaData.GetAbi()
-	if err != nil {
-		return common.Address{}, nil, nil, err
-	}
-	if parsed == nil {
-		return common.Address{}, nil, nil, errors.New("GetABI returned nil")
-	}
+     //Set new value for smart contract uint storage slot.
+     privateKey, err := crypto.HexToECDSA(os.Getenv("devTestnetPrivateKey"))
+      if err != nil {
+          log.Fatal(err)
+      }
 
-	address, tx, contract, err := bind.DeployContract(auth, *parsed, common.FromHex(MainBin), backend)
-	if err != nil {
-		return common.Address{}, nil, nil, err
-	}
-	return address, tx, &Main{MainCaller: MainCaller{contract: contract}, MainTransactor: MainTransactor{contract: contract}, MainFilterer: MainFilterer{contract: contract}}, nil
-}
+      publicKey := privateKey.Public()
+      publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+      if !ok {
+          log.Fatal("error casting public key to ECDSA")
+      }
 
-// Main is an auto generated Go binding around an Ethereum contract.
-type Main struct {
-	MainCaller     // Read-only binding to the contract
-	MainTransactor // Write-only binding to the contract
-	MainFilterer   // Log filterer for contract events
-}
+      fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+      nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+      if err != nil {
+          log.Fatal(err)
+      }
 
-// MainCaller is an auto generated read-only Go binding around an Ethereum contract.
-type MainCaller struct {
-	contract *bind.BoundContract // Generic contract wrapper for the low level calls
-}
+      gasPrice, err := client.SuggestGasPrice(context.Background())
+      if err != nil {
+          log.Fatal(err)
+      }
 
-// MainTransactor is an auto generated write-only Go binding around an Ethereum contract.
-type MainTransactor struct {
-	contract *bind.BoundContract // Generic contract wrapper for the low level calls
-}
+      auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+      if err != nil {
+          log.Fatal(err)
+      }
 
-// MainFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
-type MainFilterer struct {
-	contract *bind.BoundContract // Generic contract wrapper for the low level calls
-}
+      auth.Nonce = big.NewInt(int64(nonce))
+      auth.Value = big.NewInt(0)     // in wei
+      auth.GasLimit = uint64(300000) // in units
+      auth.GasPrice = gasPrice
 
-// MainSession is an auto generated Go binding around an Ethereum contract,
-// with pre-set call and transact options.
-type MainSession struct {
-	Contract     *Main             // Generic contract binding to set the session for
-	CallOpts     bind.CallOpts     // Call options to use throughout this session
-	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
-}
+      setUintValue := big.NewInt(111)
+      tx, err := contract.Set(auth, setUintValue)
+      if err != nil {
+          log.Fatal(err)
+      }
+      fmt.Println("Tx hash:", tx.Hash().Hex()) // tx sent
 
-// MainCallerSession is an auto generated read-only Go binding around an Ethereum contract,
-// with pre-set call options.
-type MainCallerSession struct {
-	Contract *MainCaller   // Generic contract caller binding to set the session for
-	CallOpts bind.CallOpts // Call options to use throughout this session
-}
+     //Subscribe to events from smart contract address.
+     query := ethereum.FilterQuery{
+         Addresses: []common.Address{contractAddress},
+     }
 
-// MainTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
-// with pre-set transact options.
-type MainTransactorSession struct {
-	Contract     *MainTransactor   // Generic contract transactor binding to set the session for
-	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
-}
+     logs := make(chan types.Log)
+     sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
+     if err != nil {
+         log.Fatal(err)
+     }
 
-// MainRaw is an auto generated low-level Go binding around an Ethereum contract.
-type MainRaw struct {
-	Contract *Main // Generic contract binding to access the raw methods on
-}
+     for {
+         select {
+         case err := <-sub.Err():
+             log.Fatal(err)
+         case vLog := <-logs:
+             fmt.Println("New Event Log:", vLog) // pointer to event log
 
-// MainCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
-type MainCallerRaw struct {
-	Contract *MainCaller // Generic read-only contract binding to access the raw methods on
-}
-
-// MainTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
-type MainTransactorRaw struct {
-	Contract *MainTransactor // Generic write-only contract binding to access the raw methods on
-}
-
-// NewMain creates a new instance of Main, bound to a specific deployed contract.
-func NewMain(address common.Address, backend bind.ContractBackend) (*Main, error) {
-	contract, err := bindMain(address, backend, backend, backend)
-	if err != nil {
-		return nil, err
-	}
-	return &Main{MainCaller: MainCaller{contract: contract}, MainTransactor: MainTransactor{contract: contract}, MainFilterer: MainFilterer{contract: contract}}, nil
-}
-
-// NewMainCaller creates a new read-only instance of Main, bound to a specific deployed contract.
-func NewMainCaller(address common.Address, caller bind.ContractCaller) (*MainCaller, error) {
-	contract, err := bindMain(address, caller, nil, nil)
-	if err != nil {
-		return nil, err
-	}
-	return &MainCaller{contract: contract}, nil
-}
-
-// NewMainTransactor creates a new write-only instance of Main, bound to a specific deployed contract.
-func NewMainTransactor(address common.Address, transactor bind.ContractTransactor) (*MainTransactor, error) {
-	contract, err := bindMain(address, nil, transactor, nil)
-	if err != nil {
-		return nil, err
-	}
-	return &MainTransactor{contract: contract}, nil
-}
-
-// NewMainFilterer creates a new log filterer instance of Main, bound to a specific deployed contract.
-func NewMainFilterer(address common.Address, filterer bind.ContractFilterer) (*MainFilterer, error) {
-	contract, err := bindMain(address, nil, nil, filterer)
-	if err != nil {
-		return nil, err
-	}
-	return &MainFilterer{contract: contract}, nil
-}
-
-// bindMain binds a generic wrapper to an already deployed contract.
-func bindMain(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(MainABI))
-	if err != nil {
-		return nil, err
-	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
-}
-
-// Call invokes the (constant) contract method with params as input values and
-// sets the output to result. The result type might be a single field for simple
-// returns, a slice of interfaces for anonymous returns and a struct for named
-// returns.
-func (_Main *MainRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
-	return _Main.Contract.MainCaller.contract.Call(opts, result, method, params...)
-}
-
-// Transfer initiates a plain transaction to move funds to the contract, calling
-// its default method if one is available.
-func (_Main *MainRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
-	return _Main.Contract.MainTransactor.contract.Transfer(opts)
-}
-
-// Transact invokes the (paid) contract method with params as input values.
-func (_Main *MainRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
-	return _Main.Contract.MainTransactor.contract.Transact(opts, method, params...)
-}
-
-// Call invokes the (constant) contract method with params as input values and
-// sets the output to result. The result type might be a single field for simple
-// returns, a slice of interfaces for anonymous returns and a struct for named
-// returns.
-func (_Main *MainCallerRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
-	return _Main.Contract.contract.Call(opts, result, method, params...)
-}
-
-// Transfer initiates a plain transaction to move funds to the contract, calling
-// its default method if one is available.
-func (_Main *MainTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
-	return _Main.Contract.contract.Transfer(opts)
-}
-
-// Transact invokes the (paid) contract method with params as input values.
-func (_Main *MainTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
-	return _Main.Contract.contract.Transact(opts, method, params...)
-}
-
-// StoredData is a free data retrieval call binding the contract method 0x2a1afcd9.
-//
-// Solidity: function storedData() view returns(uint256)
-func (_Main *MainCaller) StoredData(opts *bind.CallOpts) (*big.Int, error) {
-	var out []interface{}
-	err := _Main.contract.Call(opts, &out, "storedData")
-
-	if err != nil {
-		return *new(*big.Int), err
-	}
-
-	out0 := *abi.ConvertType(out[0], new(*big.Int)).(**big.Int)
-
-	return out0, err
-
-}
-
-// StoredData is a free data retrieval call binding the contract method 0x2a1afcd9.
-//
-// Solidity: function storedData() view returns(uint256)
-func (_Main *MainSession) StoredData() (*big.Int, error) {
-	return _Main.Contract.StoredData(&_Main.CallOpts)
-}
-
-// StoredData is a free data retrieval call binding the contract method 0x2a1afcd9.
-//
-// Solidity: function storedData() view returns(uint256)
-func (_Main *MainCallerSession) StoredData() (*big.Int, error) {
-	return _Main.Contract.StoredData(&_Main.CallOpts)
-}
-
-// Set is a paid mutator transaction binding the contract method 0x60fe47b1.
-//
-// Solidity: function set(uint256 x) returns()
-func (_Main *MainTransactor) Set(opts *bind.TransactOpts, x *big.Int) (*types.Transaction, error) {
-	return _Main.contract.Transact(opts, "set", x)
-}
-
-// Set is a paid mutator transaction binding the contract method 0x60fe47b1.
-//
-// Solidity: function set(uint256 x) returns()
-func (_Main *MainSession) Set(x *big.Int) (*types.Transaction, error) {
-	return _Main.Contract.Set(&_Main.TransactOpts, x)
-}
-
-// Set is a paid mutator transaction binding the contract method 0x60fe47b1.
-//
-// Solidity: function set(uint256 x) returns()
-func (_Main *MainTransactorSession) Set(x *big.Int) (*types.Transaction, error) {
-	return _Main.Contract.Set(&_Main.TransactOpts, x)
-}
-
-// MainSetEventIterator is returned from FilterSetEvent and is used to iterate over the raw logs and unpacked data for SetEvent events raised by the Main contract.
-type MainSetEventIterator struct {
-	Event *MainSetEvent // Event containing the contract specifics and raw log
-
-	contract *bind.BoundContract // Generic contract to use for unpacking event data
-	event    string              // Event name to use for unpacking event data
-
-	logs chan types.Log        // Log channel receiving the found contract events
-	sub  ethereum.Subscription // Subscription for errors, completion and termination
-	done bool                  // Whether the subscription completed delivering logs
-	fail error                 // Occurred error to stop iteration
-}
-
-// Next advances the iterator to the subsequent event, returning whether there
-// are any more events found. In case of a retrieval or parsing error, false is
-// returned and Error() can be queried for the exact failure.
-func (it *MainSetEventIterator) Next() bool {
-	// If the iterator failed, stop iterating
-	if it.fail != nil {
-		return false
-	}
-	// If the iterator completed, deliver directly whatever's available
-	if it.done {
-		select {
-		case log := <-it.logs:
-			it.Event = new(MainSetEvent)
-			if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
-				it.fail = err
-				return false
-			}
-			it.Event.Raw = log
-			return true
-
-		default:
-			return false
-		}
-	}
-	// Iterator still in progress, wait for either a data or an error event
-	select {
-	case log := <-it.logs:
-		it.Event = new(MainSetEvent)
-		if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
-			it.fail = err
-			return false
-		}
-		it.Event.Raw = log
-		return true
-
-	case err := <-it.sub.Err():
-		it.done = true
-		it.fail = err
-		return it.Next()
-	}
-}
-
-// Error returns any retrieval or parsing error occurred during filtering.
-func (it *MainSetEventIterator) Error() error {
-	return it.fail
-}
-
-// Close terminates the iteration process, releasing any pending underlying
-// resources.
-func (it *MainSetEventIterator) Close() error {
-	it.sub.Unsubscribe()
-	return nil
-}
-
-// MainSetEvent represents a SetEvent event raised by the Main contract.
-type MainSetEvent struct {
-	Raw types.Log // Blockchain specific contextual infos
-}
-
-// FilterSetEvent is a free log retrieval operation binding the contract event 0x24a4f809cc1bf5b0f3c16b615535b56a0b583b1630e06e32b46b759b8088a95d.
-//
-// Solidity: event setEvent()
-func (_Main *MainFilterer) FilterSetEvent(opts *bind.FilterOpts) (*MainSetEventIterator, error) {
-
-	logs, sub, err := _Main.contract.FilterLogs(opts, "setEvent")
-	if err != nil {
-		return nil, err
-	}
-	return &MainSetEventIterator{contract: _Main.contract, event: "setEvent", logs: logs, sub: sub}, nil
-}
-
-// WatchSetEvent is a free log subscription operation binding the contract event 0x24a4f809cc1bf5b0f3c16b615535b56a0b583b1630e06e32b46b759b8088a95d.
-//
-// Solidity: event setEvent()
-func (_Main *MainFilterer) WatchSetEvent(opts *bind.WatchOpts, sink chan<- *MainSetEvent) (event.Subscription, error) {
-
-	logs, sub, err := _Main.contract.WatchLogs(opts, "setEvent")
-	if err != nil {
-		return nil, err
-	}
-	return event.NewSubscription(func(quit <-chan struct{}) error {
-		defer sub.Unsubscribe()
-		for {
-			select {
-			case log := <-logs:
-				// New log arrived, parse the event and forward to the user
-				event := new(MainSetEvent)
-				if err := _Main.contract.UnpackLog(event, "setEvent", log); err != nil {
-					return err
-				}
-				event.Raw = log
-
-				select {
-				case sink <- event:
-				case err := <-sub.Err():
-					return err
-				case <-quit:
-					return nil
-				}
-			case err := <-sub.Err():
-				return err
-			case <-quit:
-				return nil
-			}
-		}
-	}), nil
-}
-
-// ParseSetEvent is a log parse operation binding the contract event 0x24a4f809cc1bf5b0f3c16b615535b56a0b583b1630e06e32b46b759b8088a95d.
-//
-// Solidity: event setEvent()
-func (_Main *MainFilterer) ParseSetEvent(log types.Log) (*MainSetEvent, error) {
-	event := new(MainSetEvent)
-	if err := _Main.contract.UnpackLog(event, "setEvent", log); err != nil {
-		return nil, err
-	}
-	event.Raw = log
-	return event, nil
+             storedData, err := contract.StoredData(&bind.CallOpts{})
+               if err != nil {
+                   log.Fatal(err)
+             }
+             fmt.Println("storedData:", storedData)
+         }
+     }
 }
