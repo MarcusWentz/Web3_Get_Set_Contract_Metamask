8b2d45473fdc60e461a918fa448a3969218cca24
MarcusWentz <markzcollege404@gmail.com>
2022-08-31 21:45:48 -0400

    Python script updated Goerli HTTPS

--- Scripts/python/setValueTx.py
+++ Scripts/python/setValueTx.py
@@ -2,23 +2,26 @@ from web3 import Web3
 import json
 import os
 
-infura_rinkeby_testnet_url_API =  str(os.environ['rinkebyInfuraAPIKey']);
+infura_goerli_testnet_url_API = str(os.environ['goerliHTTPS_InfuraAPIKey']);
 devTestnetPrivateKey = str(os.environ['devTestnetPrivateKey']);
-web3 = Web3(Web3.HTTPProvider(infura_rinkeby_testnet_url_API))
+web3 = Web3(Web3.HTTPProvider(infura_goerli_testnet_url_API))
 
 print("Connected to Web3? ")
 print(web3.isConnected())
 
-# Read information from the blockchain.
-print("Current block? ")
-print(web3.eth.blockNumber)
+# print("Chain ID? ")
+# print(web3.eth.chain_id())
+
+# # Read information from the blockchain.
+# print("Current block? ")
+# print(web3.eth.blockNumber)
 
 balance = web3.eth.getBalance("0xc1202e7d42655F23097476f6D48006fE56d38d4f")
-print("Balance [Rinkeby ether]" )
+print("Balance [Goerli ether]" )
 print(web3.fromWei(balance, "ether") )
 
-Contract_At_Address= web3.toChecksumAddress("0xaf3310ec212eCBA069149239F954F1281fDa836B");
-abi_Contract = json.loads('[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"date","type":"uint256"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":false,"internalType":"uint256","name":"valueChangeEventWenjs","type":"uint256"}],"name":"setValueUpdatedViaWebjs","type":"event"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]');
+Contract_At_Address= web3.toChecksumAddress("0xdbaA7dfBd9125B7a43457D979B1f8a1Bd8687f37");
+abi_Contract = json.loads('[{"anonymous":false,"inputs":[],"name":"setEvent","type":"event"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]');
 contract_Call = web3.eth.contract(address = Contract_At_Address , abi = abi_Contract);
 print(contract_Call.functions.storedData().call());
 
