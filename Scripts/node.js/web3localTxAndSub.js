49db875ce099a387632ae915dee51f5be4cfabca
MarcusWentz <markzcollege404@gmail.com>
2022-08-31 21:08:37 -0400

    Updated node.js example Goerli chain and contract

--- Scripts/node.js/web3localTxAndSub.js
+++ Scripts/node.js/web3localTxAndSub.js
@@ -3,21 +3,20 @@
 //Need to import web3 Linux:
 //sudo npm install web3
 
-//RPC LOCAL [LIKE GANACHE], TESTNET [ACTUAL BLOCKCHAIN TO CONNECT TO LIKE RINKEBY]
-var Tx = require("ethereumjs-tx").Transaction
+//RPC LOCAL [LIKE GANACHE], TESTNET [ACTUAL BLOCKCHAIN TO CONNECT TO LIKE GOERLI]
+var Tx = require("ethereumjs-tx")
 const Web3 = require('web3')
 
 const devWalletaddress = '0xc1202e7d42655F23097476f6D48006fE56d38d4f' // Your account address goes here
 const devTestnetPrivateKey = Buffer.from(process.env.devTestnetPrivateKey, 'hex')
 
-const rpcURL = process.env.rinkebyInfuraAPIKey// Your RPC URL goes here
+const rpcURL = process.env.goerliWebSocketSecureEventsInfuraAPIKey// Your RPC URL goes here
 const web3 = new Web3(rpcURL)
 
 //HIDE KEY WITH "Linux Environment Variables" https://www.youtube.com/watch?v=himEMfYQJ1w
 
-const contractAddress_JS = '0xaf3310ec212eCBA069149239F954F1281fDa836B'
-const contractABI_JS =
-[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"date","type":"uint256"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":false,"internalType":"uint256","name":"valueChangeEventWenjs","type":"uint256"}],"name":"setValueUpdatedViaWebjs","type":"event"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
+const contractAddress_JS = '0xdbaA7dfBd9125B7a43457D979B1f8a1Bd8687f37'
+const contractABI_JS = [{"anonymous":false,"inputs":[],"name":"setEvent","type":"event"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
 
 const contractDefined_JS = new web3.eth.Contract(contractABI_JS, contractAddress_JS)
 
@@ -30,16 +29,17 @@ function checkValueLatest() {
 
 function createAndSendTx() {
   // Transfer some tokens
+    const unixTIme = Date.now();
     web3.eth.getTransactionCount(devWalletaddress, (err, txCount) => {
       const txObject = {
         nonce:    web3.utils.toHex(txCount),
         gasLimit: web3.utils.toHex(30000), // Raise the gas limit to a much higher amount
         gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
         to: contractAddress_JS,
-        data: contractDefined_JS.methods.set(42).encodeABI()
+        data: contractDefined_JS.methods.set(unixTIme).encodeABI()
     }
     // Sign the transaction
-    const tx = new Tx(txObject, {chain:'rinkeby'})
+    const tx = new Tx(txObject, {chain:'goerli'})
     tx.sign(devTestnetPrivateKey)
     const serializedTx = tx.serialize()
     const raw = '0x' + serializedTx.toString('hex')
@@ -53,11 +53,11 @@ function createAndSendTx() {
 checkValueLatest();
 createAndSendTx();
 
-const web3Sub = new Web3(process.env.rinkebyWebSocketSecureEventsInfuraAPIKey)
+const web3Sub = new Web3(process.env.goerliWebSocketSecureEventsInfuraAPIKey)
 const eventsListener = new web3Sub.eth.Contract(contractABI_JS, contractAddress_JS)
 
 // //Subscribe to event.
-eventsListener.events.setValueUpdatedViaWebjs({
+eventsListener.events.setEvent({
      fromBlock: 'latest'
  }, function(error, eventResult){})
  .on('data', function(eventResult){
