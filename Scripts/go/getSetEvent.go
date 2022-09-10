4cd22357c0a8ca1367bbc10ac20495da8f68bff4
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-09-10 15:46:15 -0400

    Update getSetEvent.go

--- Scripts/go/getSetEvent.go
+++ Scripts/go/getSetEvent.go
@@ -2,7 +2,7 @@
 //Step 2: Create bin file by running: solc --bin Store.sol > Store_sol_Store.bin
 //Step 3: Remove comments above the abi and bin files.
 //Step 4: Generate Go contract interaction file by running:  abigen --bin=Store_sol_Store.bin --abi=Store_sol_Store.abi --pkg=main --out=Store.go
-//Step 5: Run: StoreTest.go Store.go
+//Step 5: Run: getSetEvent.go Store.go
 package main
 
 import (
