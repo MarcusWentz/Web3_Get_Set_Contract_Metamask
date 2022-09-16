e78af62003d8ef13b8a40ea231fb9062678fb448
MarcusWentz <markzcollege404@gmail.com>
2022-09-15 21:40:56 -0400

    Shardeum cycle olling event log examples

--- Scripts/python/ShardeumCycleBlockEventListener.py
+++ Scripts/python/ShardeumCycleBlockEventListener.py
@@ -1,8 +1,9 @@
 from web3 import Web3
-import math
-import json
 import os
 import time
+import math
+from urllib.request import urlopen
+import json
 
 ShardeumConnectionHTTPS = "https://liberty20.shardeum.org/";
 web3 = Web3(Web3.HTTPProvider(ShardeumConnectionHTTPS))
@@ -13,8 +14,29 @@ print(web3.isConnected())
 print("Chain ID? ")
 print(web3.eth.chain_id)
 
+addressToSubscribeTo = "0x0000000000000000000000000000000000000000"
+
 while True:
     print("Current cycle (1 cycle = 10 blocks [bundles]) ")
     cycle =  (math.floor(web3.eth.blockNumber/10))  #Divide current bundle [block] by 10, then round down to get cycle.
     print(cycle)
+
+    transactionsInCycleRangeUrlString = "https://explorer.liberty10.shardeum.org/api/transaction?startCycle=" + str(cycle) + "&endCycle=" + str(cycle) + "&address=" + addressToSubscribeTo
+    print(transactionsInCycleRangeUrlString)
+    transactionsInCycleRangeUrlOpened = urlopen(transactionsInCycleRangeUrlString)
+    transactionsInCycleRangeUrlJSON = json.loads(transactionsInCycleRangeUrlOpened.read())
+    totalTransactions = transactionsInCycleRangeUrlJSON["totalTransactions"]
+    print(totalTransactions)
+    pageIndex = 1
+
+    while totalTransactions > 0:
+        print(pageIndex)
+        print(totalTransactions)
+        pageIndexIncrementUrlString = transactionsInCycleRangeUrlString + "&page=" + str(pageIndex)
+        pageIndexIncrementUrlOpened = urlopen(pageIndexIncrementUrlString)
+        rawTransactionDataPage = json.loads(pageIndexIncrementUrlOpened.read())
+        print(rawTransactionDataPage)
+        totalTransactions -= 10
+        pageIndex += 1
+
     time.sleep(60)   #1 cycle roughly every 60 seconds based on explorer: https://explorer.liberty20.shardeum.org/cycle
---
+++ Scripts/python/ShardeumCycleTransactionPageReadExample.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+import json
+
+transactionsInCycleRangeUrlString = "https://explorer.liberty10.shardeum.org/api/transaction?startCycle=0&endCycle=3000&address=0x0000000000000000000000000000000000000000"
+transactionsInCycleRangeUrlOpened = urlopen(transactionsInCycleRangeUrlString)
+transactionsInCycleRangeUrlJSON = json.loads(transactionsInCycleRangeUrlOpened.read())
+totalTransactions = transactionsInCycleRangeUrlJSON["totalTransactions"]
+print(totalTransactions)
+pageIndex = 1
+
+while totalTransactions > 0:
+    print(pageIndex)
+    print(totalTransactions)
+    pageIndexIncrementUrlString = transactionsInCycleRangeUrlString + "&page=" + str(pageIndex)
+    pageIndexIncrementUrlOpened = urlopen(pageIndexIncrementUrlString)
+    rawTransactionDataPage = json.loads(pageIndexIncrementUrlOpened.read())
+    print(rawTransactionDataPage)
+    totalTransactions -= 10
+    pageIndex += 1
