f76893d707febe97c5accc23408a69fe08ff333e
MarcusWentz <markzcollege404@gmail.com>
2022-09-18 18:57:40 -0400

    Javascript example added for block listening

--- Scripts/node.js/ShardeumCycleBlockEventListener.js
+++ Scripts/node.js/ShardeumCycleBlockEventListener.js
@@ -1,3 +1,4 @@
+const https = require('https');
 const Web3 = require('web3')
 
 const rpcURL = "https://liberty20.shardeum.org/"
@@ -8,43 +9,84 @@ web3.eth.getChainId().then(console.log);
 
 let addressToSubscribeTo = "0x0000000000000000000000000000000000000000"
 
-const timeMilliSec = 1000; //2 seconds per value
+const timeMilliSec = 1000;
 
 function timeout(ms) {
 	return new Promise(resolve => setTimeout(resolve,ms));
 }
 
-async function listForCycle() {
+async function listenForCycle() {
   while (true){
 
     console.log("Current cycle (1 cycle = 10 blocks [bundles]) ")
     let cycle = await web3.eth.getBlockNumber();
-    console.log(cycle)
     console.log(Math.floor(cycle/10))
 
-    await timeout(1*timeMilliSec)
+    let totalTransactions = ""
+    let baseUrlCycleAddress = "https://explorer.liberty10.shardeum.org/api/transaction?startCycle=" + cycle + "&endCycle=" + cycle + "&address=" + addressToSubscribeTo
+
+    let req = https.get(baseUrlCycleAddress, function(res) {
+      let data = '',
+        json_data;
+
+      res.on('data', function(stream) {
+        data += stream;
+      });
+      res.on('end', function() {
+
+        json_data = JSON.parse(data);
+        totalTransactions = json_data.totalTransactions
+        console.log(totalTransactions);
+        let pageIndex = 1;
+
+        readJSONLoop(totalTransactions,baseUrlCycleAddress)
+
+      });
+    });
+
+    req.on('error', function(e) {
+        console.log(e.message);
+    });
+
+    await timeout(60*timeMilliSec)
 
   }
 }
 
-listForCycle()
+listenForCycle()
 
-/////////////////////////////////////PYTHON:
+function readJSONLoop(totalTransactions,baseUrlCycleAddress) {
 
-    // transactionsInCycleRangeUrlString = "https://explorer.liberty10.shardeum.org/api/transaction?startCycle=" + str(cycle) + "&endCycle=" + str(cycle) + "&address=" + addressToSubscribeTo
-    // print(transactionsInCycleRangeUrlString)
-    // transactionsInCycleRangeUrlOpened = urlopen(transactionsInCycleRangeUrlString)
-    // transactionsInCycleRangeUrlJSON = json.loads(transactionsInCycleRangeUrlOpened.read())
-    // totalTransactions = transactionsInCycleRangeUrlJSON["totalTransactions"]
-    // print(totalTransactions)
-    // pageIndex = 1
-    //
-    // while totalTransactions > 0:
-    //     print(pageIndex)
-    //     print(totalTransactions)
-    //     pageIndexIncrementUrlString = transactionsInCycleRangeUrlString + "&page=" + str(pageIndex)
-    //     pageIndexIncrementUrlOpened = urlopen(pageIndexIncrementUrlString)
-    //     rawTransactionDataPage = json.loads(pageIndexIncrementUrlOpened.read())
-    //     print(rawTransactionDataPage)
-    //     totalTransactions -= 10
-    //     pageIndex += 1
+	let total = totalTransactions;
+	let pageIndex = 1
+  let baseUrl = baseUrlCycleAddress;
+
+	while (total>0) {
+
+		let filterUrl = baseUrl + "&page=" + pageIndex
+		let req = https.get(filterUrl, function(res) {
+			let data = '',
+				json_data;
+
+			res.on('data', function(stream) {
+				data += stream;
+			});
+			res.on('end', function() {
+
+				json_data = JSON.parse(data);
+				console.log(json_data);
+		    let pageIndex = 1;
+
+			});
+		});
+
+		req.on('error', function(e) {
+		    console.log(e.message);
+		});
+
+	  total -= 10;
+		pageIndex++;
+		console.log(filterUrl)
+	}
+
+}
