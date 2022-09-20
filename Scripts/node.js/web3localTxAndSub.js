d2f81afc007d291c7f63ef94edae7abf262e6fef
MarcusWentz <markzcollege404@gmail.com>
2022-09-20 13:42:52 -0400

    Javascript Goerli ethers send Tx example

--- Scripts/node.js/web3localTxAndSub.js
+++ Scripts/node.js/web3localTxAndSub.js
@@ -1,63 +1,48 @@
-//Source material: https://www.dappuniversity.com/articles/web3-js-intro
-
-//Need to import web3 Linux:
-//sudo npm install web3
-
-//RPC LOCAL [LIKE GANACHE], TESTNET [ACTUAL BLOCKCHAIN TO CONNECT TO LIKE GOERLI]
 const Web3 = require('web3')
-var Tx = require("ethereumjs-tx")
+const ethers = require("ethers")
 
-const rpcURL = process.env.goerliWebSocketSecureEventsInfuraAPIKey// Your RPC URL goes here
+const rpcURL = process.env.goerliHTTPS_InfuraAPIKey// Your RPC URL goes here
 const web3 = new Web3(rpcURL)
 
-console.log("chainId:")
-web3.eth.getChainId().then(console.log);
+const provider = new ethers.providers.JsonRpcProvider(rpcURL)
+const signer = new ethers.Wallet(Buffer.from(process.env.devTestnetPrivateKey, 'hex'), provider);
 
-const devTestnetPrivateKey = Buffer.from(process.env.devTestnetPrivateKey, 'hex')
-const devWalletAddress = web3.eth.accounts.privateKeyToAccount(process.env.devTestnetPrivateKey).address;
+const contractAddress = '0xdbaA7dfBd9125B7a43457D979B1f8a1Bd8687f37'
+const contractABI = [{"anonymous":false,"inputs":[],"name":"setEvent","type":"event"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
 
-//HIDE KEY WITH "Linux Environment Variables" https://www.youtube.com/watch?v=himEMfYQJ1w
+const contractDeployed = new web3.eth.Contract(contractABI, contractAddress)
 
-const contractAddress_JS = '0xdbaA7dfBd9125B7a43457D979B1f8a1Bd8687f37'
-const contractABI_JS = [{"anonymous":false,"inputs":[],"name":"setEvent","type":"event"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
-
-const contractDefined_JS = new web3.eth.Contract(contractABI_JS, contractAddress_JS)
-
-//Check if value was set
-function checkValueLatest() {
-  contractDefined_JS.methods.storedData().call((err, balance) => {
-    console.log({ err, balance })
-  })
-}
-
-function createAndSendTx() {
-  // Transfer some tokens
-    const unixTIme = Date.now();
-    web3.eth.getTransactionCount(devWalletAddress, (err, txCount) => {
-      const txObject = {
-        nonce:    web3.utils.toHex(txCount),
-        gasLimit: web3.utils.toHex(30000), // Raise the gas limit to a much higher amount
-        gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
-        to: contractAddress_JS,
-        data: contractDefined_JS.methods.set(unixTIme).encodeABI()
-    }
-    // Sign the transaction
-    const tx = new Tx(txObject, {chain:'goerli'})
-    tx.sign(devTestnetPrivateKey)
-    const serializedTx = tx.serialize()
-    const raw = '0x' + serializedTx.toString('hex')
-    // Broadcast the transaction hash and check for errors
-      web3.eth.sendSignedTransaction(raw, (err, txHash) => {
-      console.log('err:', err, 'txHash:', txHash)
-    })
-  })
-}
-
-checkValueLatest();
 createAndSendTx();
 
+async function createAndSendTx() {
+
+  const chainIdConnected = await web3.eth.getChainId();
+  console.log("chainIdConnected: "+chainIdConnected)
+
+  const storedData = await contractDeployed.methods.storedData().call()
+  console.log("storedData: "+storedData)
+
+  const unixTIme = Date.now();
+
+  const txCount = await provider.getTransactionCount(signer.address);
+
+  const tx = signer.sendTransaction({
+    // chainId: chainIdConnected,
+    nonce:    web3.utils.toHex(txCount),
+    gasLimit: web3.utils.toHex(30000), // Raise the gas limit to a much higher amount
+    gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
+    to: contractAddress,
+    data: contractDeployed.methods.set(unixTIme).encodeABI()
+  });
+
+  await tx
+  console.log(tx)
+
+
+}
+
 const web3Sub = new Web3(process.env.goerliWebSocketSecureEventsInfuraAPIKey)
-const eventsListener = new web3Sub.eth.Contract(contractABI_JS, contractAddress_JS)
+const eventsListener = new web3Sub.eth.Contract(contractABI, contractAddress)
 
 // //Subscribe to event.
 eventsListener.events.setEvent({
