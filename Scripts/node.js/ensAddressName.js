4f5616ebc5c40c48f0dd9e04b539275539b5aa50
MarcusWentz <markzcollege404@gmail.com>
2022-09-22 12:01:52 -0400

    ENS address name WeiBridged wallet connect ethers test

---
+++ Scripts/node.js/ensAddressName.js
@@ -0,0 +1,16 @@
+const ethers = require("ethers")
+
+const rpcURL = process.env.goerliHTTPS_InfuraAPIKey// Your RPC URL goes here
+
+const provider = new ethers.providers.JsonRpcProvider(rpcURL)
+const signer = new ethers.Wallet(Buffer.from(process.env.devTestnetPrivateKey, 'hex'), provider);
+
+createAndSendTx();
+
+async function createAndSendTx() {
+
+  const address = '0xc1202e7d42655F23097476f6D48006fE56d38d4f';
+  const name = await provider.lookupAddress(address);
+  console.log(name)
+
+}
