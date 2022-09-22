aebf7a041e8be661b8a0c106bce00726c1b110f4
MarcusWentz <markzcollege404@gmail.com>
2022-09-22 17:19:10 -0400

    ENS name example updated

--- Scripts/node.js/ensAddressName.js
+++ Scripts/node.js/ensAddressName.js
@@ -1,13 +1,13 @@
 const ethers = require("ethers")
 
-const rpcURL = process.env.goerliHTTPS_InfuraAPIKey// Your RPC URL goes here
+const rpcURL = process.env.goerliHTTPS_InfuraAPIKey // Your RPC URL goes here
 
 const provider = new ethers.providers.JsonRpcProvider(rpcURL)
 const signer = new ethers.Wallet(Buffer.from(process.env.devTestnetPrivateKey, 'hex'), provider);
 
-createAndSendTx();
+addressLookupNameENS();
 
-async function createAndSendTx() {
+async function addressLookupNameENS() {
 
   const address = '0xc1202e7d42655F23097476f6D48006fE56d38d4f';
   const name = await provider.lookupAddress(address);
