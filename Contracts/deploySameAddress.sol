9fc9f5b5cf7eba26fdaff1807960089b4bbd3269
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-10-21 12:51:39 -0400

    Update deploySameAddress.sol

--- Contracts/deploySameAddress.sol
+++ Contracts/deploySameAddress.sol
@@ -9,29 +9,31 @@ contract ContractToDeploy {
     uint public storageSlot0;
     address public immutable Owner;
 
-    constructor(uint inputValueDeployed) payable {
+    constructor(address inputAddress) payable {
         if(msg.value != 1 ether) { revert msgValueNotOneEther(); }
-        Owner = msg.sender;
-        storageSlot0 = inputValueDeployed;
+        Owner = inputAddress; //Need to put address as input instead of msg.sender because msg.sender will be another contract with CREATE2 salt deploy.
     }
     
-    function changeValue(uint updatedValue) public {
-        storageSlot0 = updatedValue;
+    function changeValue(uint updateStorageSlot) public {
+        storageSlot0 = updateStorageSlot;
     }
 
     function killThisContract() public {
         if(msg.sender != Owner) { revert notOwner(); }
         payable(Owner).transfer(address(this).balance);
-        selfdestruct(payable(address(this))); //Deletes all data from contract.
+        selfdestruct(payable(address(this))); //Deletes all data from this contract.
     }
 
 }
 
 contract Create2Factory { //Modified from: "Create2 | Solidity 0.8" by  Smart Contract Programmer https://www.youtube.com/watch?v=883-koWrsO4&ab_channel=SmartContractProgrammer
 
-    function create2(bytes32 _salt, uint inputValueDeployed) public payable {
+    address public lastDeployedAddress;
+
+    function create2(bytes32 _salt, address ownerAddress) public payable { // Salt example: 0x0000000000000000000000000000000000000000000000000000000000000000
         if(msg.value != 1 ether) { revert msgValueNotOneEther(); }
-        new ContractToDeploy {value: 1 ether, salt: _salt}(inputValueDeployed);
+        ContractToDeploy deployedAddress = new ContractToDeploy {value: 1 ether, salt: _salt}(ownerAddress);
+        lastDeployedAddress = address(deployedAddress);
     }
 
 }
