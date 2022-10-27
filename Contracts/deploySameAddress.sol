4e33c3119a96b4a2fcdad930f1f56f03c2c2b968
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-10-27 15:13:39 -0400

    Update deploySameAddress.sol

--- Contracts/deploySameAddress.sol
+++ Contracts/deploySameAddress.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.17;
 
 error notOwner();
-error msgValueNotOneEther();
 
 contract ContractToDeploy {
 
@@ -10,7 +9,6 @@ contract ContractToDeploy {
     address public immutable Owner;
 
     constructor(address inputAddress) payable {
-        if(msg.value != 1 ether) { revert msgValueNotOneEther(); }
         Owner = inputAddress; //Need to put address as input instead of msg.sender because msg.sender will be another contract with CREATE2 salt deploy.
     }
     
@@ -20,8 +18,7 @@ contract ContractToDeploy {
 
     function killThisContract() public { //After a contract is killed at an address, you can deploy another contract at that address.
         if(msg.sender != Owner) { revert notOwner(); }
-        payable(Owner).transfer(address(this).balance);
-        selfdestruct(payable(address(this))); //Deletes all data from this contract.
+        selfdestruct(payable(Owner)); //Deletes all data from this contract.
     }
 
 }
@@ -31,14 +28,13 @@ contract Create2Factory { //Modified from: "Create2 | Solidity 0.8" by  Smart Co
     address public lastDeployedAddress;
 
     function create2(bytes32 _salt, address ownerAddress) public payable { // Salt example:         0x0000000000000000000000000000000000000000000000000000000000000000
-        if(msg.value != 1 ether) { revert msgValueNotOneEther(); }         // Another salt example: 0x123456789abcdef0000000000000000000000000000000000000000000000000
         ContractToDeploy deployedAddress = new ContractToDeploy {value: 1 ether, salt: _salt}(ownerAddress);
         lastDeployedAddress = address(deployedAddress);
     }
 
     function precomputeAddress(uint _salt, address ownerAddress) public view returns (address) {
-        bytes memory bytecode = abi.encodePacked(type(ContractToDeploy).creationCode, abi.encode(ownerAddress));
-        bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), _salt, keccak256(bytecode)));
+        bytes memory creationCodeValue = abi.encodePacked(type(ContractToDeploy).creationCode, abi.encode(ownerAddress));
+        bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), _salt, keccak256(creationCodeValue)));
         return address(uint160(uint(hash)));
     }
 
