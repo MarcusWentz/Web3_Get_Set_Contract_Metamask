095fbd212ab22eaa66ac3c2902dab8b31016d53f
MarcusWentz <markzcollege404@gmail.com>
2022-12-10 06:19:12 -0500

    Rust cleanup read

--- Scripts/rust/test_ethers/src/main.rs
+++ Scripts/rust/test_ethers/src/main.rs
@@ -1,58 +1,23 @@
 //Credit: https://coinsbench.com/ethereum-with-rust-tutorial-part-1-create-simple-transactions-with-rust-26d365a7ea93
-// use std::time::Duration;
 use std::convert::TryFrom;
 use std::env;
 
-// use ethers::utils::Ganache;
-// use ethers::signers::{Signer, LocalWallet};
 use ethers::signers::{LocalWallet};
 
 use ethers_providers::{Middleware, Provider, Http};
-// use ethers_core::types::Address;
-// use ethers_core::types::{Address,TransactionRequest,U256};
-use ethers_core::types::{Address,TransactionRequest};
+use ethers_core::types::{Address};
+// use ethers_core::types::{Address,TransactionRequest};
 
 use ethers::middleware::SignerMiddleware;
-// use ethers_signers::{Wallet};
-// use ethers_providers::{Provider, Http};
-
-// use ethers_core::rand::thread_rng;
-
-
 
 use eyre::Result;
 
-
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Spawn a aanache instance
-    // let ganache = Ganache::new().spawn();
-    // println!("HTTP Endpoint: {}", ganache.endpoint());
-    //
-    // let mnemonic = "gas monster ski craft below illegal discover limit dog bundle bus artefact";
-    // let ganache = Ganache::new().mnemonic(mnemonic).spawn();
-    //
-    // // // Get the first wallet managed by ganache
-    // let wallet: LocalWallet = ganache.keys()[0].clone().into();
-    // println!("Wallet object: {:?}", wallet);
-    // let wallet_address = wallet.address();
-    // let wallet_address_raw: String = "0xb5f27c716e44ffe48fd6622983c651355ad8c75a".to_string();
-    // println!("Wallet address hex: {:?}", wallet_address);
-    // println!("Wallet address string: {}", wallet_address_raw);
-
-    // A provider is an Ethereum JsonRPC client
-    // let provider = Provider::try_from(ganache.endpoint())?.interval(Duration::from_millis(10));
-    // println!("provider: {:?}", provider);
 
     let rpc_goerli_infura_https = env::var("goerliHTTPS_InfuraAPIKey").expect("$goerliHTTPS_InfuraAPIKey is not set");
 
     let private_key_wallet_string = env::var("devTestnetPrivateKey").expect("$devTestnetPrivateKey is not set");
-
-    //
-    // let str_addr: &str = private_key_wallet_string;
-    // let addr: Address = str_addr.parse().unwrap()
-
-    // let private_key_wallet_hex : Address = private_key_wallet_string.parse().unwrap();
 
     let provider = Provider::<Http>::try_from(rpc_goerli_infura_https).expect("could not instantiate HTTP Provider");
     // provider.set_chain(&mut 5);
@@ -62,28 +27,16 @@ async fn main() -> Result<()> {
     let signer: LocalWallet = private_key_wallet_string.parse().expect("fail parse");
     // let signer = signer.set_chain_id(5u64);
 
+    // let signer = signer.set_chain_id(1337u64);
+
     println!("signer {:?}:", signer);
-    // println!("signer {:?}:", signer.chain_id());
+    // println!("signer {:?}:", signer.chain_Id);
 
-
-    // let wallet = "380eb0f3d505f087e438eca80bc4df9a7faa24f868e69fc0440261a0fc0567dc".parse()?;
+    // let signer = signer.set_chain_id(1337u64);
 
     let client = SignerMiddleware::new(provider, signer);
 
-    // By default, signing of messages and transactions is done locally
-    // (transactions will be broadcast via the eth_sendRawTransaction API)
-    // let wallet: Wallet = "380eb0f3d505f087e438eca80bc4df9a7faa24f868e69fc0440261a0fc0567dc".parse()?;
-
-
-    // let provider = Provider::<Http>::try_from(rpc_goerli_infura_https).unwrap().with_sender(addr);
-
-
-    // let client = Wallet::new(private_key_wallet).connect(provider);
-    // println!("client {:?}",client);
-    //
-    //
-    // let provider = Provider::<Http>::try_from(anvil.endpoint()).unwrap().with_sender(private_key_wallet);
-
+    println!("client {:?}:", client);
 
     let block = client.get_block(0).await?;
     println!("Got block: {:?}", block);
@@ -102,25 +55,16 @@ async fn main() -> Result<()> {
     let ens_balance = client.get_balance("car.eth", None).await?;
     println!("ENS address balance: {}", ens_balance);
 
-    // let wallet = Wallet::new(&mut thread_rng());
-    // println!("Wallet {:?}",wallet);
-
-    // // Create a transaction to transfer 1000 wei to `other_address`
-    // let tx = TransactionRequest::pay(other_address, U256::from(1000u64)).from(first_address);
-    // // Send the transaction and wait for receipt
-    // let _receipt = client.send_transaction(tx, None);
-
-    // let _tx2 = TransactionRequest::new()
-    //     .chain_id(5)
+    // let _tx = TransactionRequest::new()
+    //     // .chain_id(1)
     //     .to(other_address)
     //     .value(1000);
-    // let _tx_hash2 = client.send_transaction(_tx2, None).await?;
+    // let _tx_hash = client.send_transaction(_tx, None).await?;
     //
-    // println!("_tx_hash2 {:?}: ", _tx_hash2);
-
-
-    println!("Balance for address {}: {}",first_address_hex, first_balance);
-    println!("Balance for address {}: {}",other_address_hex, other_balance);
+    // println!("_tx_hash {:?}: ", _tx_hash);
+    //
+    // println!("Balance for address {}: {}",first_address_hex, first_balance);
+    // println!("Balance for address {}: {}",other_address_hex, other_balance);
 
     Ok(())
 
