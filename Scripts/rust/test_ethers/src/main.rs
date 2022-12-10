444db79f1b8c44888f8f782a25a34895ee3e61a2
MarcusWentz <markzcollege404@gmail.com>
2022-12-10 04:53:52 -0500

    Rust client setup but the chainId is wrong

--- Scripts/rust/test_ethers/src/main.rs
+++ Scripts/rust/test_ethers/src/main.rs
@@ -4,12 +4,21 @@ use std::convert::TryFrom;
 use std::env;
 
 // use ethers::utils::Ganache;
-use ethers::signers::{Wallet, Signer, LocalWallet};
+// use ethers::signers::{Signer, LocalWallet};
+use ethers::signers::{LocalWallet};
+
 use ethers_providers::{Middleware, Provider, Http};
 // use ethers_core::types::Address;
-use ethers_core::types::{Address,TransactionRequest,U256};
+// use ethers_core::types::{Address,TransactionRequest,U256};
+use ethers_core::types::{Address,TransactionRequest};
 
-use ethers_core::rand::thread_rng;
+use ethers::middleware::SignerMiddleware;
+// use ethers_signers::{Wallet};
+// use ethers_providers::{Provider, Http};
+
+// use ethers_core::rand::thread_rng;
+
+
 
 use eyre::Result;
 
@@ -46,6 +55,25 @@ async fn main() -> Result<()> {
     // let private_key_wallet_hex : Address = private_key_wallet_string.parse().unwrap();
 
     let provider = Provider::<Http>::try_from(rpc_goerli_infura_https).expect("could not instantiate HTTP Provider");
+    // provider.set_chain(&mut 5);
+
+    println!("provider {:?}:", provider);
+
+    let signer: LocalWallet = private_key_wallet_string.parse().expect("fail parse");
+    // let signer = signer.set_chain_id(5u64);
+
+    println!("signer {:?}:", signer);
+    // println!("signer {:?}:", signer.chain_id());
+
+
+    // let wallet = "380eb0f3d505f087e438eca80bc4df9a7faa24f868e69fc0440261a0fc0567dc".parse()?;
+
+    let client = SignerMiddleware::new(provider, signer);
+
+    // By default, signing of messages and transactions is done locally
+    // (transactions will be broadcast via the eth_sendRawTransaction API)
+    // let wallet: Wallet = "380eb0f3d505f087e438eca80bc4df9a7faa24f868e69fc0440261a0fc0567dc".parse()?;
+
 
     // let provider = Provider::<Http>::try_from(rpc_goerli_infura_https).unwrap().with_sender(addr);
 
@@ -57,32 +85,40 @@ async fn main() -> Result<()> {
     // let provider = Provider::<Http>::try_from(anvil.endpoint()).unwrap().with_sender(private_key_wallet);
 
 
-    let block = provider.get_block(0).await?;
+    let block = client.get_block(0).await?;
     println!("Got block: {:?}", block);
 
     // Query the balance of our account
     let first_address_hex = "0xc1202e7d42655F23097476f6D48006fE56d38d4f";
     let first_address = "0xc1202e7d42655F23097476f6D48006fE56d38d4f".parse::<Address>()?;
-    let first_balance = provider.get_balance(first_address, None).await?;
+    let first_balance = client.get_balance(first_address, None).await?;
     println!("Balance for address {}: {}",first_address_hex, first_balance);
 
     let other_address_hex = "0x66C1d8A5ee726b545576A75380391835F8AAA43c";
     let other_address = "0x66C1d8A5ee726b545576A75380391835F8AAA43c".parse::<Address>()?;
-    let other_balance = provider.get_balance(other_address, None).await?;
+    let other_balance = client.get_balance(other_address, None).await?;
     println!("Balance for address {}: {}",other_address_hex, other_balance);
 
-    let ens_balance = provider.get_balance("car.eth", None).await?;
+    let ens_balance = client.get_balance("car.eth", None).await?;
     println!("ENS address balance: {}", ens_balance);
 
-    let wallet = Wallet::new(&mut thread_rng());
-    println!("Wallet {:?}",wallet);
+    // let wallet = Wallet::new(&mut thread_rng());
+    // println!("Wallet {:?}",wallet);
 
-    // let wallet = wallet.private_key(private_key_wallet);
+    // // Create a transaction to transfer 1000 wei to `other_address`
+    // let tx = TransactionRequest::pay(other_address, U256::from(1000u64)).from(first_address);
+    // // Send the transaction and wait for receipt
+    // let _receipt = client.send_transaction(tx, None);
 
-    // Create a transaction to transfer 1000 wei to `other_address`
-    let tx = TransactionRequest::pay(other_address, U256::from(1000u64)).from(first_address);
-    // Send the transaction and wait for receipt
-    let _receipt = provider.send_transaction(tx, None);
+    // let _tx2 = TransactionRequest::new()
+    //     .chain_id(5)
+    //     .to(other_address)
+    //     .value(1000);
+    // let _tx_hash2 = client.send_transaction(_tx2, None).await?;
+    //
+    // println!("_tx_hash2 {:?}: ", _tx_hash2);
+
+
     println!("Balance for address {}: {}",first_address_hex, first_balance);
     println!("Balance for address {}: {}",other_address_hex, other_balance);
 
