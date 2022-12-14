e8581c71c27fc3b5f31d7b3e55ce7156ab716821
MarcusWentz <markzcollege404@gmail.com>
2022-12-14 12:05:22 -0500

    Rust connect client with chainId from provider then send ether

---
+++ Scripts/rust/test_ethers_write/src/main.rs
@@ -0,0 +1,50 @@
+//Credit: https://coinsbench.com/ethereum-with-rust-tutorial-part-1-create-simple-transactions-with-rust-26d365a7ea93
+use std::convert::TryFrom;
+use std::env;
+
+use ethers_signers::{LocalWallet, Signer};
+use ethers_providers::{Middleware, Provider, Http};
+use ethers_core::types::{Address,TransactionRequest};
+use ethers_middleware::SignerMiddleware;
+// use ethers_core::types::{Address, TransactionRequest};
+
+use eyre::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    let rpc_goerli_infura_https = env::var("goerliHTTPS_InfuraAPIKey").expect("$goerliHTTPS_InfuraAPIKey is not set");
+
+    let private_key_wallet_string = env::var("devTestnetPrivateKey").expect("$devTestnetPrivateKey is not set");
+
+    let provider = Provider::<Http>::try_from(rpc_goerli_infura_https).expect("could not instantiate HTTP Provider");
+
+    println!("provider {:?}:", provider);
+
+    let signer = private_key_wallet_string.parse::<LocalWallet>()?;
+
+    let signer_address = signer.address();
+    println!("signer address: {}", signer_address);
+
+    println!("signer {:?}:", signer);
+
+    let block = provider.get_block(0).await?;
+    println!("Got block: {:?}", block);
+
+    let other_address_hex = "0x0000000000000000000000000000000000000000";
+    let other_address = "0x0000000000000000000000000000000000000000".parse::<Address>()?;
+    let other_balance = provider.get_balance(other_address, None).await?;
+    println!("Balance for address {}: {}",other_address_hex, other_balance);
+
+    //Setup client with chainId from provider: https://github.com/gakonst/ethers-rs/issues/1873#issuecomment-1324248559
+    let client = SignerMiddleware::new_with_provider_chain(provider, signer).await.unwrap();
+    println!("client {:?}:", client);
+
+    // ...and sign transactions
+    let tx = TransactionRequest::pay(signer_address, 100);
+    let pending_tx = client.send_transaction(tx, None).await?;
+    let _receipt = pending_tx.confirmations(2).await?;
+
+    Ok(())
+
+}
