e2a14450cec6a954b96e725e5007028ef1e53f7f
marcuswentz <markzcollege404@gmail.com>
2022-12-27 00:25:08 -0500

    Rust read balances no wallet import

--- Scripts/rust/test_ethers_read_ether/src/main.rs
+++ Scripts/rust/test_ethers_read_ether/src/main.rs
@@ -2,7 +2,6 @@
 use std::convert::TryFrom;
 use std::env;
 
-use ethers_signers::{LocalWallet, Signer};
 use ethers_providers::{Middleware, Provider, Http};
 use ethers_core::types::{Address};
 
@@ -13,21 +12,7 @@ async fn main() -> Result<()> {
 
     let rpc_goerli_infura_https = env::var("goerliHTTPS_InfuraAPIKey").expect("$goerliHTTPS_InfuraAPIKey is not set");
 
-    let private_key_wallet_string = env::var("devTestnetPrivateKey").expect("$devTestnetPrivateKey is not set");
-
     let provider = Provider::<Http>::try_from(rpc_goerli_infura_https).expect("could not instantiate HTTP Provider");
-
-    println!("provider {:?}:", provider);
-
-    let signer = private_key_wallet_string.parse::<LocalWallet>()?;
-
-    let signer_address = signer.address();
-    println!("signer address: {}", signer_address);
-
-    println!("signer {:?}:", signer);
-
-    let block = provider.get_block(0).await?;
-    println!("Got block: {:?}", block);
 
     let other_address_hex = "0x0000000000000000000000000000000000000000";
     let other_address = "0x0000000000000000000000000000000000000000".parse::<Address>()?;
