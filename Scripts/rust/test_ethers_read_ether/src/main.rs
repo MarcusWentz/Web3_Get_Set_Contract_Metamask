7d0ccb6bfd4ef18df8a5cbf42a28891073303719
marcuswentz <markzcollege404@gmail.com>
2022-12-31 18:07:08 -0500

    Rust read Option unwrap Block object parameters

--- Scripts/rust/test_ethers_read_ether/src/main.rs
+++ Scripts/rust/test_ethers_read_ether/src/main.rs
@@ -14,12 +14,30 @@ async fn main() -> Result<()> {
 
     // let rpc_shardeum_https = "https://liberty20.shardeum.org/";
 
-    // let rpc_shardeum_local_http = "http://localhost:8080";
+    // let rpc_goerli_infura_https = "http://localhost:8080";
 
     let provider = Provider::<Http>::try_from(rpc_goerli_infura_https).expect("could not instantiate HTTP Provider");
 
     let chainid = provider.get_chainid().await?;
     println!("Got chainid: {}", chainid);
+
+    let block = provider.get_block(1000000u64).await?;
+    println!("Got block: {:?}", block);
+
+    let block_timestamp = block.clone().unwrap().timestamp;
+    println!("Got block_timestamp: {:?}", block_timestamp);
+
+    let block_number = block.clone().unwrap().number.unwrap();
+    println!("Got block_number: {:?}", block_number);
+
+    let block_gas = block.clone().unwrap().gas_used;
+    println!("Got block_gas: {:?}", block_gas);
+    
+    let block_gas_limit = block.clone().unwrap().gas_limit;
+    println!("Got block_gas_limit: {:?}", block_gas_limit);
+
+    let block_hash = block.clone().unwrap().hash.unwrap();
+    println!("Got block_hash: {:?}", block_hash);
 
     let other_address_hex = "0x0000000000000000000000000000000000000000";
     let other_address = "0x0000000000000000000000000000000000000000".parse::<Address>()?;
