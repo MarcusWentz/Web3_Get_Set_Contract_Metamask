519f2f93f19a9251cc61caa0ab864e934a9751a4
marcuswentz <markzcollege404@gmail.com>
2023-01-09 20:59:57 -0500

    Rust websocket send string messages from server to client

--- Scripts/rust/test_websocket_server_client/src/main.rs
+++ Scripts/rust/test_websocket_server_client/src/main.rs
@@ -9,10 +9,10 @@ use tungstenite::accept;
 fn main() {
 
     println!("Websocket server running on:");
-    println!("ws://0.0.0.0:8000")
-    println!("Open a terminal and run a websocket client with:")
-    println!("'websocat ws://0.0.0.0:8000'")
-    println!("When the client connects, type something, hit enter, and the server will send the message back to the client terminal.")
+    println!("ws://0.0.0.0:8000");
+    println!("Open a terminal and run a websocket client with:");
+    println!("'websocat ws://0.0.0.0:8000'");
+    println!("When the client connects, type something, hit enter, and the server will send the message back to the client terminal.");
 
     let listener = TcpListener::bind(("0.0.0.0", 8000)).unwrap();
     for stream in listener.incoming() {
@@ -24,6 +24,8 @@ fn main() {
 
                 // Just echo back everything that the client sent to us
                 if msg.is_binary() || msg.is_text() {
+                    println!("Client sent server: {}", msg);
+                    websocket.write_message("Sent the same message back to client".into()).unwrap();
                     websocket.write_message(msg).unwrap();
                 }
             }
