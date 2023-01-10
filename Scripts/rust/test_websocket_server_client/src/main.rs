8930ed93a7b83fe73db3377d75433cb5bd766c90
marcuswentz <markzcollege404@gmail.com>
2023-01-09 20:51:58 -0500

    Rust coroutine websocket server and client messages

---
+++ Scripts/rust/test_websocket_server_client/src/main.rs
@@ -0,0 +1,33 @@
+#[macro_use]
+extern crate may;
+extern crate native_tls;
+extern crate tungstenite;
+
+use may::net::TcpListener;
+use tungstenite::accept;
+
+fn main() {
+
+    println!("Websocket server running on:");
+    println!("ws://0.0.0.0:8000")
+    println!("Open a terminal and run a websocket client with:")
+    println!("'websocat ws://0.0.0.0:8000'")
+    println!("When the client connects, type something, hit enter, and the server will send the message back to the client terminal.")
+
+    let listener = TcpListener::bind(("0.0.0.0", 8000)).unwrap();
+    for stream in listener.incoming() {
+        go!(move || {
+            let mut websocket = accept(stream.unwrap()).unwrap();
+
+            loop {
+                let msg = websocket.read_message().unwrap();
+
+                // Just echo back everything that the client sent to us
+                if msg.is_binary() || msg.is_text() {
+                    websocket.write_message(msg).unwrap();
+                }
+            }
+        });
+
+    }
+}
