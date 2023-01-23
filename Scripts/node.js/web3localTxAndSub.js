31222fef1c917871d17af4f9f2d07d7005385da2
marcuswentz <markzcollege404@gmail.com>
2023-01-23 11:46:28 -0500

    ethers.js listen to events with subscription and remove web3.js

--- Scripts/node.js/web3localTxAndSub.js
+++ Scripts/node.js/web3localTxAndSub.js
@@ -1,11 +1,8 @@
-const Web3 = require('web3')
 const ethers = require("ethers")
 
-const rpcURL = process.env.goerliHTTPS_InfuraAPIKey// Your RPC URL goes here
+const rpcURL = process.env.goerliHTTPS_InfuraAPIKey // Your RPC URL goes here
 
 // const rpcURL = "http://localhost:8545"// Your RPC URL goes here
-
-const web3 = new Web3(rpcURL)
 
 const provider = new ethers.providers.JsonRpcProvider(rpcURL)
 const signer = new ethers.Wallet(Buffer.from(process.env.devTestnetPrivateKey, 'hex'), provider);
@@ -17,65 +14,45 @@ const contractABI = [{"anonymous":false,"inputs":[],"name":"setEvent","type":"ev
 
 const contractDeployed = new ethers.Contract(contractAddress, contractABI, signer);
 
-createAndSendTx();
+createAndSendTx()
+getStoredData()
+
+async function getStoredData() {  
+  const storedData = await contractDeployed.storedData()
+  console.log("storedData: "+ storedData)
+}
 
 async function createAndSendTx() {
-
-  // const chainIdConnected = await web3.eth.getChainId();
-  // console.log("chainIdConnected: "+chainIdConnected)
 
   const connectedNetworkObject = await provider.getNetwork();
   const chainIdConnected = connectedNetworkObject.chainId;
   console.log("chainIdConnected: "+ chainIdConnected)
-
-  const storedData = await contractDeployed.storedData()
-  console.log("storedData: "+ storedData)
 
   const unixTIme = Date.now();
 
   const txCount = await provider.getTransactionCount(signer.address);
 
   const callDataObject = await contractDeployed.populateTransaction.set(unixTIme);
-  const tx_data = callDataObject.data;
+  const txData = callDataObject.data;
 
-  // const callDataObject = await contractDeployed.populateTransaction.set(unixTIme);
-
-  const tx = signer.sendTransaction({
+  const txSigned = signer.sendTransaction({
     chainId: chainIdConnected,
     to: contractAddress,
-    // to: signer.address,
     nonce:    txCount,
     gasLimit: ethers.utils.hexlify(210000), // Raise the gas limit to a much higher amount
     gasPrice: ethers.utils.hexlify(10000000000),
-    data: tx_data
+    data: txData
   });
 
-  await tx
-  console.log(tx)
+  await txSigned
+  console.log(txSigned)
 
 }
 
-const web3Sub = new Web3(process.env.goerliWebSocketSecureEventsInfuraAPIKey)
+contractDeployed.on("setEvent", () => {
 
-// const web3Sub = new Web3("ws://localhost:8546")
+  console.log("EVENT DETECTED! NEW STATE VALUE: ")
 
-const eventsListener = new web3Sub.eth.Contract(contractABI, contractAddress)
+  getStoredData()
 
-// //Subscribe to event.
-eventsListener.events.setEvent({
-     fromBlock: 'latest'
- }, function(error, eventResult){})
- .on('data', function(eventResult){
-    //console.log(eventResult)
-   //Call the get function to get the most accurate present state for the value.
-   console.log("EVENT DETECTED! NEW STATE VALUE: ")
-
-   eventsListener.methods.storedData().call((err, balance) => {
-     console.log({ err, balance })
-   })
-
-   })
- .on('changed', function(eventResult){
-     // remove event from local database
- })
- .on('error', console.error);
+});
