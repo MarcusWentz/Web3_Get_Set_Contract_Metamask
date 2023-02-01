de35a87ebb1a179350b07ef2052670529fc29923
marcuswentz <markzcollege404@gmail.com>
2023-01-31 22:11:01 -0500

    Transaction receipt test

--- Scripts/javascript/eth_rpc_testing/eth_randomTests.js
+++ Scripts/javascript/eth_rpc_testing/eth_randomTests.js
@@ -2,13 +2,17 @@ const ethers = require("ethers");
 (async () => {
 
   // Goerli
-  // const provider = new ethers.providers.JsonRpcProvider(process.env.goerliHTTPS_InfuraAPIKey)
+  const provider = new ethers.providers.JsonRpcProvider(process.env.goerliHTTPS_InfuraAPIKey)
 
   // //Liberty 2.X
-  const provider = new ethers.providers.JsonRpcProvider("https://liberty20.shardeum.org/")
+  // const provider = new ethers.providers.JsonRpcProvider("https://liberty20.shardeum.org/")
   
   
   //TEST GOES HERE
+  const txReceipt = await provider.waitForTransaction( 
+    "0x8229ac8d9ecfa6ea4d9c5696a8ab2c463455ca0e9f99f0d351615b42784700eb"
+  );
+  console.log(txReceipt);
 
 
 })();
