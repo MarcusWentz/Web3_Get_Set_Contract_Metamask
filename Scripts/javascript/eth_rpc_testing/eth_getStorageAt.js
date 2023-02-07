03b2e754dbfffa4d5dbcd16e8485d34a3f83d033
marcuswentz <markzcollege404@gmail.com>
2023-02-06 19:31:56 -0500

    Test bytecode getCode fails test

--- Scripts/javascript/eth_rpc_testing/eth_getStorageAt.js
+++ Scripts/javascript/eth_rpc_testing/eth_getStorageAt.js
@@ -4,7 +4,7 @@ const ethers = require("ethers");
 // const provider = new ethers.providers.JsonRpcProvider(process.env.goerliHTTPS_InfuraAPIKey)
 // const contractAddress = '0x080FfD52b6c217C1B69a03446f2956580e25fd43'
 
-//Liberty 2.X
+//Betanet 1.X
 const provider = new ethers.providers.JsonRpcProvider("https://sphinx.shardeum.org/")
 const contractAddress = '0x9228dA2e3724A12b4B0d2d621ea501829671cf52'
 
