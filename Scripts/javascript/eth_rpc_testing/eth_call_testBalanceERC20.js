08f1641dab58e3ea78b2fb4381a7e71ab3b99c76
marcuswentz <markzcollege404@gmail.com>
2023-02-07 00:06:33 -0500

    eth_call raw Chainlink token balance return working

---
+++ Scripts/javascript/eth_rpc_testing/eth_call_testBalanceERC20.js
@@ -0,0 +1,30 @@
+const ethers = require("ethers");
+(async () => {
+
+  // Goerli
+  // const provider = new ethers.providers.JsonRpcProvider(process.env.goerliHTTPS_InfuraAPIKey)
+  // const chainLinkTokenAddress = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
+
+  //Betanet 1.X
+  const provider = new ethers.providers.JsonRpcProvider("https://sphinx.shardeum.org/")
+  const chainLinkTokenAddress = "0xdbaA7dfBd9125B7a43457D979B1f8a1Bd8687f37";
+
+  const fromWallet = "0x66C1d8A5ee726b545576A75380391835F8AAA43c";
+  const sliceWalletToAddToData = fromWallet.slice(2,42);
+  console.log(sliceWalletToAddToData)
+
+  const hexStringFunction = ethers.utils.toUtf8Bytes("balanceOf(address)")
+  const hexStringFunctionHashed = ethers.utils.keccak256(hexStringFunction)
+  const functionSelector = hexStringFunctionHashed.slice(0,10);
+
+  let callData = functionSelector + "000000000000000000000000" + sliceWalletToAddToData
+  console.log(callData);  
+
+  let callReturnData = await provider.call({
+    from: fromWallet, 
+    to: chainLinkTokenAddress,
+    data: callData
+  });
+  console.log(callReturnData);
+  
+})();
--- Scripts/javascript/eth_rpc_testing/eth_randomTests.js
+++ Scripts/javascript/eth_rpc_testing/eth_randomTests.js
@@ -5,10 +5,8 @@ const ethers = require("ethers");
   // const provider = new ethers.providers.JsonRpcProvider(process.env.goerliHTTPS_InfuraAPIKey)
 
   // //Liberty 2.X
-  const provider = new ethers.providers.JsonRpcProvider("http://45.79.181.218:8080/")
+  const provider = new ethers.providers.JsonRpcProvider("https://liberty20.shardeum.org/")
     
-
-
   const filter = {
     address: "0xc1202e7d42655F23097476f6D48006fE56d38d4f"
   };
