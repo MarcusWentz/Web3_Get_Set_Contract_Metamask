51dad295e222254bc0e50d9d6fe71abd426c0daf
marcuswentz <markzcollege404@gmail.com>
2023-02-08 12:26:47 -0500

    Update test

--- Contracts/bulkTransfers/bulkTransferWithBatchERC1155.sol
+++ Contracts/bulkTransfers/bulkTransferWithBatchERC1155.sol
@@ -14,7 +14,7 @@ contract MultipleFungibleTokens is ERC1155 { //ERC1155 cannot also be a ERC1155H
 
 }
 
-contract bulkSenderERC1155 is ERC1155Holder { //Approve tokens to this contract for calling safeTransferFrom
+contract bulkSenderERC1155 { //Approve tokens to this contract for calling safeTransferFrom
 
     function bulkTransfer(address nftAddress, address[] calldata toList, uint256[] calldata tokenIds, uint256[] calldata amounts, bytes calldata data) external {
         for (uint256 i = 0; i < toList.length;) {
@@ -29,6 +29,8 @@ contract BurnTokensERC1155 is ERC1155Holder {}
 
 //Test inputs:
 
+//setApprovalForAll(addressBulkSenderERC1155,true)
+
 //Betanet 1.X
 //(
 // addressBulkSenderERC1155
@@ -38,19 +40,13 @@ contract BurnTokensERC1155 is ERC1155Holder {}
 // "0x"
 //)
 
-//Remix VM
-//(
-// addressBulkSenderERC1155
-// ["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",addressBurnTokensERC1155],
-// ["0","1","2"]
-// ["1000000000000000000","2000000000000000000","3000000000000000000"],
-// "0x"
-//)
+//Remix VM Test:
 
-//Remix VM
+//setApprovalForAll(0xE3Ca443c9fd7AF40A2B5a95d43207E763e56005F,true)
+
 //(
-// "0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47",
-// ["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"],
+// 0xE3Ca443c9fd7AF40A2B5a95d43207E763e56005F,
+// ["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",0x9a2E12340354d2532b4247da3704D2A5d73Bd189],
 // ["0","1","2"]
 // ["1000000000000000000","2000000000000000000","3000000000000000000"],
 // "0x"
