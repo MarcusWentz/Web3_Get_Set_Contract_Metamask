421fc0631d8270a319f05ee484f59ebafa8ec96d
marcuswentz <markzcollege404@gmail.com>
2023-02-14 01:11:33 -0500

    Rust RPC method eth_call working ERC-20 balanceOf(address) Chainlink Goerli

--- Scripts/javascript/eth_rpc_testing/eth_call_testBalanceERC20.js
+++ Scripts/javascript/eth_rpc_testing/eth_call_testBalanceERC20.js
@@ -2,12 +2,12 @@ const ethers = require("ethers");
 (async () => {
 
   // Goerli
-  // const provider = new ethers.providers.JsonRpcProvider(process.env.goerliHTTPS_InfuraAPIKey)
-  // const chainLinkTokenAddress = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
+  const provider = new ethers.providers.JsonRpcProvider(process.env.goerliHTTPS_InfuraAPIKey)
+  const chainLinkTokenAddress = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
 
   //Betanet 1.X
-  const provider = new ethers.providers.JsonRpcProvider("https://sphinx.shardeum.org/")
-  const chainLinkTokenAddress = "0xdbaA7dfBd9125B7a43457D979B1f8a1Bd8687f37";
+  // const provider = new ethers.providers.JsonRpcProvider("https://sphinx.shardeum.org/")
+  // const chainLinkTokenAddress = "0xdbaA7dfBd9125B7a43457D979B1f8a1Bd8687f37";
 
   const fromWallet = "0x66C1d8A5ee726b545576A75380391835F8AAA43c";
   const sliceWalletToAddToData = fromWallet.slice(2,42);
---
+++ Scripts/rust/test_ethers_eth_call/src/main.rs
@@ -0,0 +1,54 @@
+use std::env;
+
+use std::str::FromStr;
+
+use ethers::types::transaction::eip2718::TypedTransaction;
+
+use ethers_providers::{Provider};
+use ethers_core::types::{Address};
+
+
+use ethers::{
+    prelude::*,
+};
+
+use eyre::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    let rpc_goerli_infura_https = env::var("goerliHTTPS_InfuraAPIKey").expect("$goerliHTTPS_InfuraAPIKey is not set");
+
+    let provider = Provider::<Http>::try_from(rpc_goerli_infura_https).expect("could not instantiate HTTP Provider");
+
+    let contract_address = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB".parse::<Address>()?; //Chainlink Address Goerli
+
+    let raw_call_data = Bytes::from_str("0x70a0823100000000000000000000000066C1d8A5ee726b545576A75380391835F8AAA43c").unwrap(); //ERC-20: balanceOf(0x66C1d8A5ee726b545576A75380391835F8AAA43c)
+
+    let mut tx_raw = TypedTransaction::Legacy(TransactionRequest::new());
+    tx_raw.set_to(contract_address);
+    tx_raw.set_data(raw_call_data);
+    tx_raw.set_chain_id(5);
+
+    println!("{:?}", tx_raw);
+
+    // let rpc_shardeum_https = "https://liberty20.shardeum.org/";
+
+    // let provider = Provider::<Http>::try_from(rpc_shardeum_https).expect("could not instantiate HTTP Provider");
+
+    // let contract_address = "0x8f01876ccd727fd703787ed477b7f71e1e3adbb1".parse::<Address>()?; //0xdbaA7dfBd9125B7a43457D979B1f8a1Bd8687f37
+
+    // let raw_call_data = Bytes::from(b"8da5cb5b");
+
+    // let mut tx_raw = TypedTransaction::Legacy(TransactionRequest::new());
+    // tx_raw.set_to(contract_address);
+    // tx_raw.set_data(raw_call_data);
+
+    let call_return_data = provider.call(&tx_raw,None).await?;
+    
+    println!("{:?}", call_return_data);
+    
+    Ok(())
+
+}
+
