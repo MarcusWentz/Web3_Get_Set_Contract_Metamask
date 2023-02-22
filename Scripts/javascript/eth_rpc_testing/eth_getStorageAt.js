d5ab8431a996dc10c6866bd40ba6e17b44ac100b
marcuswentz <markzcollege404@gmail.com>
2023-02-22 13:17:21 -0500

    Update storage test with Liberty 1.X and 2.X

--- Scripts/javascript/eth_rpc_testing/eth_getStorageAt.js
+++ Scripts/javascript/eth_rpc_testing/eth_getStorageAt.js
@@ -1,12 +1,20 @@
 const ethers = require("ethers");
 
 //Goerli
-// const provider = new ethers.providers.JsonRpcProvider(process.env.goerliHTTPS_InfuraAPIKey)
-// const contractAddress = '0x080FfD52b6c217C1B69a03446f2956580e25fd43'
+const provider = new ethers.providers.JsonRpcProvider(process.env.goerliHTTPS_InfuraAPIKey)
+const contractAddress = '0x080FfD52b6c217C1B69a03446f2956580e25fd43'
 
 //Betanet 1.X
-const provider = new ethers.providers.JsonRpcProvider("https://sphinx.shardeum.org/")
-const contractAddress = '0x9228dA2e3724A12b4B0d2d621ea501829671cf52'
+// const provider = new ethers.providers.JsonRpcProvider("https://sphinx.shardeum.org/")
+// const contractAddress = '0x8414F1BaC5fCdA2C274A4a78D0D62109f1Cbb6C8'
+
+//Liberty 2.X
+// const provider = new ethers.providers.JsonRpcProvider("https://liberty20.shardeum.org/")
+// const contractAddress = '0xC8b70cCB981Ac7a19b6b8d21f172d97231539575'
+
+//Liberty 1.X
+// const provider = new ethers.providers.JsonRpcProvider("https://liberty10.shardeum.org/")
+// const contractAddress = '0x5eCe667D03F29695937F23178aBad9B89434D630'
 
 const storageSlot = 0;
 
