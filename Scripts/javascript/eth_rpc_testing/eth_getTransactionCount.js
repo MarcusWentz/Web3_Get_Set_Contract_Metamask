2ed36b165e650caefb80f05776084956962a787b
marcuswentz <markzcollege404@gmail.com>
2023-03-07 14:35:53 -0500

    Javascript ethers.js gas oracle scaled BigInt

--- Scripts/javascript/eth_rpc_testing/eth_getTransactionCount.js
+++ Scripts/javascript/eth_rpc_testing/eth_getTransactionCount.js
@@ -1,18 +1,24 @@
 const ethers = require("ethers");
 
 // Goerli
-const provider = new ethers.providers.JsonRpcProvider(process.env.goerliHTTPS_InfuraAPIKey)
-const contractAddress = '0x080FfD52b6c217C1B69a03446f2956580e25fd43'
+// const provider = new ethers.providers.JsonRpcProvider(process.env.goerliHTTPS_InfuraAPIKey)
+// const contractAddress = '0x080FfD52b6c217C1B69a03446f2956580e25fd43'
 
 // Betanet 1.X
-// const provider = new ethers.providers.JsonRpcProvider("https://sphinx.shardeum.org/")
-// const contractAddress = '0xD55b6893779d64756Ed0162579f54F2Fc83bAC24'
+const provider = new ethers.providers.JsonRpcProvider("https://sphinx.shardeum.org/")
+const contractAddress = '0xD55b6893779d64756Ed0162579f54F2Fc83bAC24'
 
 const contractABI = [{"inputs":[],"name":"sameStorageValue","type":"error"},{"anonymous":false,"inputs":[],"name":"setOpenDataEvent","type":"event"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
 
 const signer = new ethers.Wallet(Buffer.from(process.env.devTestnetPrivateKey, 'hex'), provider);
 
 const contractDeployed = new ethers.Contract(contractAddress, contractABI, signer);
+
+const timeMilliSec = 1000;
+
+function timeout(ms) {
+	return new Promise(resolve => setTimeout(resolve,ms));
+}
 
 transactionCountTest()
 
@@ -26,16 +32,50 @@ async function transactionCountTest() {
 
   const storedDataValue = await contractDeployed.storedData()
 
-  try{
-    await contractDeployed.set(storedDataValue); //Will compute the gas limit opcodes automatically and get the oracle gas price per gas unit.
-  } catch {
-      console.log("Transaction failed as expected.")
-  }
+  // try{
 
-const txCountAfter = await provider.getTransactionCount(
-  "0xc1202e7d42655F23097476f6D48006fE56d38d4f",
-  "latest"
-);
-console.log(txCountAfter);
+    // If you do this, the transaction will not be sent since it will predict the transaction getting reverted.
+    // await contractDeployed.set(storedDataValue); //Will compute the gas limit opcodes automatically and get the oracle gas price per gas unit.
+
+    //Send a raw transaction to force the failed transaction being sent.
+    const connectedNetworkObject = await provider.getNetwork();
+    const chainIdConnected = connectedNetworkObject.chainId;
+    console.log("chainIdConnected: "+ chainIdConnected)
+
+    const txCount = await provider.getTransactionCount(signer.address); 
+
+    const callDataObject = await contractDeployed.populateTransaction.set(storedDataValue);
+    const txData = callDataObject.data;
+
+    const gasPriceOracle = await provider.getGasPrice();
+
+    console.log(gasPriceOracle)
+
+    const gasPriceScaled = (BigInt(gasPriceOracle)*BigInt(110))/(BigInt(100))
+
+    console.log(gasPriceScaled)
+
+    const txSigned = await signer.sendTransaction({
+      chainId: chainIdConnected,
+      to: contractAddress,
+      nonce:    txCount,
+      gasLimit: ethers.utils.hexlify(285000), // Raise the gas limit to a much higher amount
+      gasPrice: gasPriceScaled,
+      data: txData
+    });
+
+    console.log(txSigned.hash)
+
+    await timeout(36*timeMilliSec)
+  
+  // } catch {
+  //     console.log("Transaction failed.")
+  // }
+
+  const txCountAfter = await provider.getTransactionCount(
+    "0xc1202e7d42655F23097476f6D48006fE56d38d4f",
+    "latest"
+  );
+  console.log(txCountAfter);
 
 }
