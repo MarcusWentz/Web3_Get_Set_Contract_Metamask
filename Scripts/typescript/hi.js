f03b300ea6515d9238af90a709c9a1e4ae618f20
marcuswentz <markzcollege404@gmail.com>
2023-03-14 14:51:28 -0400

    Typescript hello world run and compile

---
+++ Scripts/typescript/hi.js
@@ -0,0 +1,6 @@
+// greeter.ts
+var textToPrint = "Print this text.";
+printText(textToPrint);
+function printText(person) {
+    console.log(person);
+}
---
+++ Scripts/typescript/hi.ts
@@ -0,0 +1,14 @@
+// Run with 
+// ts-node hi.ts
+
+// Compile to javascript with 
+// tsc hi.ts
+
+const textToPrint = "Print this text."
+
+printText(textToPrint)
+
+function printText(person: string) {
+    console.log(person)
+}
+      
