25d9cf1f009c0871ba5c2560c9face2d39434698
marcuswentz <markzcollege404@gmail.com>
2023-03-15 23:31:52 -0400

    Shardeum eth_getBlockByNumber test Typescript

---
+++ Scripts/typescript/shardeum_rpc_method_block_cycle_emulation/eth_getBlockByNumber.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+test_eth_getBlockReceipts(42620)
+
+async function eth_getBlockByNumber(cycleToEmulateBlock: number) {
+
+	let baseUrl = 
+    "https://explorer-sphinx.shardeum.org/api/cycleinfo?from=" + 
+    cycleToEmulateBlock.toString() +
+    "&to=" +
+    cycleToEmulateBlock.toString();
+    
+    // console.log(baseUrl)
+
+    let responseRawJSON = await axios.get(baseUrl);
+    let responseDataJSON = responseRawJSON.data;
+    return responseDataJSON
+
+}
+
+async function test_eth_getBlockReceipts(cycleToEmulateBlock: number){
+
+  let cycleData = await eth_getBlockByNumber(cycleToEmulateBlock);
+  console.log("JSON object: ", cycleData);
+  console.log("Raw string response: ", JSON.stringify(cycleData));
+
+}
