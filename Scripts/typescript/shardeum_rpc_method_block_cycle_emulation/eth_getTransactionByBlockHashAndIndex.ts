5ad756a263e7936843614a8b1dd13250ef6476ca
marcuswentz <markzcollege404@gmail.com>
2023-03-19 15:16:33 -0400

    Remove Bootstrap write raw CSS

---
+++ Scripts/typescript/shardeum_rpc_method_block_cycle_emulation/eth_getTransactionByBlockHashAndIndex.ts
@@ -0,0 +1,119 @@
+import axios from "axios";
+
+test_eth_getTransactionByBlockNumberAndIndex()
+
+async function eth_getBlockByHash(cycleToEmulateBlockHash: String) {
+
+  if(cycleToEmulateBlockHash.slice(0,2) != "0x" ){
+    console.log("Block hash should start with 0x!")
+    return;
+  }
+
+  if(cycleToEmulateBlockHash.length != 66){
+    console.log("Block hash is not 66 characters long!")
+    return 
+  }
+
+	let baseUrl = 
+    "https://explorer-sphinx.shardeum.org/api/cycleinfo?marker=" + 
+    cycleToEmulateBlockHash.slice(2,66); //Hide the 0x for the Explorer API since it is not used.
+    
+  console.log(baseUrl)
+
+    let responseRawJSON = await axios.get(baseUrl);
+    let responseDataJSON = responseRawJSON.data;
+    return responseDataJSON
+
+}
+
+async function eth_getBlockTransactionCountByNumber(cycleToEmulateBlock: number) {
+
+	let baseUrl = 
+		"https://explorer-sphinx.shardeum.org/api/transaction?startCycle=" + 
+		cycleToEmulateBlock.toString() + 
+		"&endCycle=" + 
+		cycleToEmulateBlock.toString();
+    
+    let responseRawJSON = await axios.get(baseUrl);
+    let responseDataJSON = responseRawJSON.data;
+    let totalTransactionsValue = responseDataJSON.totalTransactions; 
+    return totalTransactionsValue
+
+}
+
+// let transactionArray = [];
+
+async function eth_getBlockReceipts(cycleToEmulateBlock: number): Promise<any[]> {
+
+  // let total = await eth_getBlockTransactionCountByNumber(cycleToEmulateBlock);
+  let transactionsInCycle = await eth_getBlockTransactionCountByNumber(cycleToEmulateBlock);
+  // let total = 11;
+  console.log("Total transactions for cycle 2000: " + transactionsInCycle)
+  let pageIndex = 1
+  let arrayTransactionsRaw = [];
+
+	let baseUrl = 
+		"https://explorer-sphinx.shardeum.org/api/transaction?startCycle=" + 
+		cycleToEmulateBlock.toString() + 
+		"&endCycle=" + 
+		// 2001 ;
+    cycleToEmulateBlock.toString() ;
+
+  while (transactionsInCycle > 0) {
+
+    console.log("TOTAL",transactionsInCycle);
+
+    let filterPageIncrement = baseUrl + "&page=" + pageIndex
+      
+    let responseRawJSON = await axios.get(filterPageIncrement);
+    let responseDataJSON = responseRawJSON.data;
+    let responseTransactionDataJSON = responseDataJSON.transactions;
+    // let totalTransactionsValue = responseDataJSON.totalTransactions; 
+  
+    // console.log(responseTransactionDataJSON);
+
+    let loopArrayOnPage = 0;
+    while(loopArrayOnPage<10) {
+      let transactionPointer = responseTransactionDataJSON[loopArrayOnPage];
+      if (transactionPointer === undefined) {
+        let arrayTransactionsInOrder = arrayTransactionsRaw.reverse();
+        console.log("No more transactions on page.");
+        console.log("Transaction array length: ", arrayTransactionsInOrder.length)
+        // console.log("Transaction array first tx (9/9, index 8 since we count from 0): ", arrayTransactionsInOrder[8])
+        // console.log("Transaction array first tx (1/9, index 0 since we count from 0): ", arrayTransactionsInOrder[0])
+        return arrayTransactionsInOrder;
+      } else {
+        arrayTransactionsRaw.push(transactionPointer);
+        // console.log(transactionPointer);
+      }
+      loopArrayOnPage++;
+    }
+    transactionsInCycle -= 10;
+    pageIndex++;
+
+    // console.log(filterPageIncrement)
+  }
+
+  return []; //Used to hide compiler error even though we never hit this line since we return in a while loop above.
+}
+
+async function eth_getTransactionByHashNumberAndIndex(blockHash: string, txIndex: number){
+  let cycleData = await eth_getBlockByHash(
+    "0xf2a9a9aa1e71d24f0f072f7ee3a2a46df1dd5b30ecdf44aa4742372470ebd1a4"
+  );
+  let cycleNumber = cycleData.cycles[0].counter;
+  let arrayTransactionsInOrder =  await eth_getBlockReceipts(cycleNumber);
+  return arrayTransactionsInOrder[txIndex];
+}
+
+async function test_eth_getTransactionByBlockNumberAndIndex() {
+
+  let txIndex =  await eth_getTransactionByHashNumberAndIndex(
+    "0xf2a9a9aa1e71d24f0f072f7ee3a2a46df1dd5b30ecdf44aa4742372470ebd1a4", //Cycle 2000
+    8 // Transaction 9/9 (index 8 we are counting from 0).
+  ); 
+  console.log(txIndex); //Raw Tx
+  console.log("Tx Id: " + txIndex.txId); 
+  console.log("Tx Hash: 0x" + txIndex.txId); 
+
+}
