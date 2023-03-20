4bfb07450b6783e32df91f438477b323a4e3bce2
marcuswentz <markzcollege404@gmail.com>
2023-03-20 14:59:50 -0400

    ShardeumCycleTransactionLogIndex axios

--- Scripts/javascript/shardeum_testing/eventPolling/ShardeumCycleTransactionLogIndex.js
+++ Scripts/javascript/shardeum_testing/eventPolling/ShardeumCycleTransactionLogIndex.js
@@ -1,27 +1,20 @@
-const https = require('https');
+const axios = require('axios');
 
-let totalTransactions = ""
 let baseUrl = "https://explorer-sphinx.shardeum.org/api/transaction?txHash=0xc36b485971a68b1371126d1d5f93002bd40f8fd29c861a8cc59709d20f162b78"
 
-let req = https.get(baseUrl, function(res) {
-	let data = '',
-		json_data;
+test_transaction_event_logIndex(baseUrl)
 
-	res.on('data', function(stream) {
-		data += stream;
-	});
-	res.on('end', function() {
+async function test_transaction_event_logIndex(baseUrl) {
 
-		transactionDataJSON = JSON.parse(data);
-		transactionDataReadableReceiptJSON = transactionDataJSON.transactions[0].wrappedEVMAccount.readableReceipt
-		console.log("event one() emitted at logIndex:", + transactionDataReadableReceiptJSON.logs[0].logIndex );
-		console.log("event two() emitted at logIndex:", + transactionDataReadableReceiptJSON.logs[1].logIndex );
-		console.log("event three() emitted at logIndex:", + transactionDataReadableReceiptJSON.logs[2].logIndex );
-		console.log("event four() emitted at logIndex:", + transactionDataReadableReceiptJSON.logs[3].logIndex );
+	let responseRawJSON = await axios.get(baseUrl);
+	let responseDataJSON = responseRawJSON.data;
 
-	});
-});
+	transactionDataReadableReceiptJSON = responseDataJSON.transactions[0].wrappedEVMAccount.readableReceipt
+	console.log("event one() emitted at logIndex:", + transactionDataReadableReceiptJSON.logs[0].logIndex );
+	console.log("event two() emitted at logIndex:", + transactionDataReadableReceiptJSON.logs[1].logIndex );
+	console.log("event three() emitted at logIndex:", + transactionDataReadableReceiptJSON.logs[2].logIndex );
+	console.log("event four() emitted at logIndex:", + transactionDataReadableReceiptJSON.logs[3].logIndex );
 
-req.on('error', function(e) {
-    console.log(e.message);
-});
+}
+
+
