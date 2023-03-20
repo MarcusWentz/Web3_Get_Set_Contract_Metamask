5bcad201f695f9a4fdc99bc212548227f6dfb34a
marcuswentz <markzcollege404@gmail.com>
2023-03-20 14:40:20 -0400

    Event polling Axios and ethers.js every 60 seconds

--- Scripts/javascript/shardeum_testing/eventPolling/ShardeumCycleBlockEventListener.js
+++ Scripts/javascript/shardeum_testing/eventPolling/ShardeumCycleBlockEventListener.js
@@ -1,15 +1,13 @@
-const https = require('https');
-const Web3 = require('web3')
+const axios = require('axios');
+const ethers = require('ethers')
 
 const rpcURL = "https://liberty20.shardeum.org/"
-const web3 = new Web3(rpcURL)
 
-console.log("chainId:")
-web3.eth.getChainId().then(console.log);
-
-let addressToSubscribeTo = "0x0000000000000000000000000000000000000000"
+const provider = new ethers.providers.JsonRpcProvider(rpcURL)
 
 const timeMilliSec = 1000;
+
+listenForCycle()
 
 function timeout(ms) {
 	return new Promise(resolve => setTimeout(resolve,ms));
@@ -19,75 +17,40 @@ async function listenForCycle() {
   while (true){
 
     console.log("Current cycle (1 cycle = 10 blocks [bundles]) ")
-    let cycle = await web3.eth.getBlockNumber();
+    let cycle = await provider.getBlockNumber();
     console.log(Math.floor(cycle/10))
 
-    let totalTransactions = ""
-    let baseUrlCycleAddress = "https://explorer.liberty20.shardeum.org/api/transaction?startCycle=" + cycle + "&endCycle=" + cycle + "&address=" + addressToSubscribeTo
+    let baseUrlCycleAddress = "https://explorer-liberty20.shardeum.org/api/transaction?startCycle=" + cycle + "&endCycle=" + cycle + "&address=0x0000000000000000000000000000000000000000" 
     console.log(baseUrlCycleAddress)
 
-    let req = https.get(baseUrlCycleAddress, function(res) {
-      let data = '',
-        json_data;
+    let responseRawJSON = await axios.get(baseUrlCycleAddress);
+    let responseDataJSON = responseRawJSON.data;
+    let totalTransactions = responseDataJSON.totalTransactions
+    console.log(totalTransactions);
 
-      res.on('data', function(stream) {
-        data += stream;
-      });
-      res.on('end', function() {
-
-        json_data = JSON.parse(data);
-        totalTransactions = json_data.totalTransactions
-        console.log(totalTransactions);
-        let pageIndex = 1;
-
-        readJSONLoop(totalTransactions,baseUrlCycleAddress)
-
-      });
-    });
-
-    req.on('error', function(e) {
-        console.log(e.message);
-    });
+    readJSONLoopLatestCycle(totalTransactions,baseUrlCycleAddress)
 
     await timeout(60*timeMilliSec)
 
   }
 }
 
-listenForCycle()
-
-function readJSONLoop(totalTransactions,baseUrlCycleAddress) {
+async function readJSONLoopLatestCycle(totalTransactions,baseUrl) {
 
 	let total = totalTransactions;
 	let pageIndex = 1
-  let baseUrl = baseUrlCycleAddress;
 
-	while (total>0) {
+	while ( total > 0 ) {
 
 		let filterUrl = baseUrl + "&page=" + pageIndex
-		let req = https.get(filterUrl, function(res) {
-			let data = '',
-				json_data;
-
-			res.on('data', function(stream) {
-				data += stream;
-			});
-			res.on('end', function() {
-
-				json_data = JSON.parse(data);
-				console.log(json_data);
-		    let pageIndex = 1;
-
-			});
-		});
-
-		req.on('error', function(e) {
-		    console.log(e.message);
-		});
-
-	  total -= 10;
-		pageIndex++;
 		console.log(filterUrl)
+
+		let responseRawJSON = await axios.get(filterUrl);
+		responseRawJSON = responseRawJSON.data;
+		console.log(responseRawJSON);
+
+    total -= 10;
+		pageIndex++;
 	}
 
 }
