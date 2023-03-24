478326e4ac2d62b0404231248760690450e5d402
marcuswentz <markzcollege404@gmail.com>
2023-03-24 13:10:09 -0400

    eth_getUncleByBlockNumberAndIndex and eth_getUncleByBlockHashAndIndex tests

---
+++ Scripts/javascript/eth_rpc_testing/eth_getUncleByBlockHashAndIndex.js
@@ -0,0 +1,18 @@
+const ethers = require("ethers");
+
+(async () => {
+
+  // const provider = new ethers.providers.JsonRpcProvider("https://sphinx.shardeum.org/");
+
+  const provider = new ethers.providers.JsonRpcProvider(process.env.mainnetHTTPS_QuicknodeAPIKey);
+  
+  const uncleCount = await provider.send("eth_getUncleByBlockHashAndIndex", 
+   [
+    "0xa917fcc721a5465a484e9be17cda0cc5493933dd3bc70c9adbee192cb419c9d7",
+    "0x0"
+    // "0x1"
+   ]
+  );
+  console.log(uncleCount);
+
+})();
---
+++ Scripts/javascript/eth_rpc_testing/eth_getUncleByBlockNumberAndIndex.js
@@ -0,0 +1,18 @@
+const ethers = require("ethers");
+
+(async () => {
+
+  // const provider = new ethers.providers.JsonRpcProvider("https://sphinx.shardeum.org/");
+
+  const provider = new ethers.providers.JsonRpcProvider(process.env.mainnetHTTPS_QuicknodeAPIKey);
+  
+  const uncleCount = await provider.send("eth_getUncleByBlockNumberAndIndex", 
+   [
+    "0xc5043f",
+    "0x0"
+    // "0X1"
+   ]
+  );
+  console.log(uncleCount);
+
+})();
