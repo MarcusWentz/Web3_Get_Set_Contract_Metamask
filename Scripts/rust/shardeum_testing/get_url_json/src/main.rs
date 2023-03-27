c7785e3b962089d764ef5f2ff417d399f61381c5
marcuswentz <markzcollege404@gmail.com>
2023-03-27 12:39:19 -0400

    Rust loop through pages handle overflow underflow error with signed int

--- Scripts/rust/shardeum_testing/get_url_json/src/main.rs
+++ Scripts/rust/shardeum_testing/get_url_json/src/main.rs
@@ -8,11 +8,8 @@ async fn main() -> Result<(), reqwest::Error> {
 
     let transaction_count : u64 = get_transaction_count(cycle_number.clone(), base_url.clone()).await;    
     println!("transaction_count: {:#?}", transaction_count);
-
-    // transactionCount := getTransactionCount(6928, baseUrl)
-    // log.Println(transactionCount)
      
-    //readJsonLoop(transactionCount, baseUrl)
+    read_json_loop(cycle_number.clone(), base_url.clone(), transaction_count).await;
 
     Ok(())
 }
@@ -41,3 +38,35 @@ async fn get_transaction_count(cycle_number: u64, base_url: String) -> u64   {
     return new_todo["totalTransactions"].as_u64().unwrap();
 
 }
+
+async fn read_json_loop(cycle_number: u64, base_url: String, total_transactions: u64) {
+
+   let mut total : i64 = total_transactions as i64; //Convert value to be signed so we do not have an underflow error when the value become negative.
+   let mut page_index = 1;
+
+   while total > 0 {
+
+        let get_request_url = 
+            base_url.clone() +
+            &cycle_number.to_string() +
+            "&endCycle=" +
+            &cycle_number.to_string() +
+            "&page=" + 
+            &page_index.to_string();
+        println!("getRequestUrl: {:#?}", get_request_url);
+
+        let new_todo: serde_json::Value = reqwest::Client::new()
+            .get(get_request_url)
+            .send()
+            .await.unwrap()
+            .json()
+            .await.unwrap();
+    
+        println!("JSON raw response: {:#?}", new_todo);
+
+        total -= 10;
+        page_index += 1;
+    }   
+    
+}
+
