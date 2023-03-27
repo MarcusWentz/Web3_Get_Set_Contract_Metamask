debd65159919e7677dc49d7ef2bf26f6eea19693
marcuswentz <markzcollege404@gmail.com>
2023-03-26 20:18:30 -0400

    Get transaction count from Sharduem cycle Rust

--- Scripts/rust/shardeum_testing/get_url_json/src/main.rs
+++ Scripts/rust/shardeum_testing/get_url_json/src/main.rs
@@ -3,16 +3,38 @@ use serde_json;
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
 
+    let base_url = String::from("https://explorer-sphinx.shardeum.org/api/transaction?startCycle=");
+    let cycle_number = 6928;
+
+    get_transaction_count(cycle_number.clone(), base_url.clone()).await?;    
+    // transactionCount := getTransactionCount(6928, baseUrl)
+    // log.Println(transactionCount)
+     
+    //readJsonLoop(transactionCount, baseUrl)
+
+    Ok(())
+}
+
+async fn get_transaction_count(cycle_number: u64, base_url: String) -> Result<(), reqwest::Error>  {
+
+    let get_request_url = 
+        base_url +
+        &cycle_number.to_string() +
+        "&endCycle=" +
+        &cycle_number.to_string();
+    println!("getRequestUrl: {:#?}", get_request_url);
+
     let new_todo: serde_json::Value = reqwest::Client::new()
-        .get("https://explorer-sphinx.shardeum.org/api/transaction?startCycle=6928&endCycle=6928")
-        .send()
-        .await?
-        .json()
-        .await?;
+    .get(get_request_url)
+    .send()
+    .await?
+    .json()
+    .await?;
 
     println!("JSON raw response: {:#?}", new_todo);
     println!("{:#?}", new_todo["success"]);
     println!("{:#?}", new_todo["totalTransactions"]);
 
     Ok(())
+
 }
