a77aa877448ebd94c9d478eda90a106f89c5e0b1
marcuswentz <markzcollege404@gmail.com>
2023-04-07 21:08:33 -0400

    Token pair test

--- Scripts/javascript/shardeum_testing/UniswapV2/ethPair/addLiquidityEthRouter.js
+++ Scripts/javascript/shardeum_testing/UniswapV2/ethPair/addLiquidityEthRouter.js
@@ -11,13 +11,13 @@ const addressUniswapV2Router02 = '0xd49d79d476215BEF1E5AC43c46eC9dB6E7906dbD'
 const provider = new ethers.providers.JsonRpcProvider(rpcURL)
 const signer = new ethers.Wallet(Buffer.from(process.env.devTestnetPrivateKey, 'hex'), provider);
 
-const abiUniswapV2Router02 = JSON.parse( fs.readFileSync('abiUniswapV2Router02.json') );
+const abiUniswapV2Router02 = JSON.parse( fs.readFileSync('../abiUniswapV2Router02.json') );
 const abiIERC20 = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]
 
 const contractUniswapV2Router02 = new ethers.Contract(addressUniswapV2Router02, abiUniswapV2Router02, signer);
 const contractIERC20 = new ethers.Contract(tokenERC20Address, abiIERC20, signer);
 
-UniswapV2RouterSwapExactETHForTokens()
+UniswapV2RouterAddLiquidityETH()
 
 async function getWrappedTokenAddress() {  
   const storedData = await contractUniswapV2Router02.WETH()
@@ -29,12 +29,12 @@ async function getFactoryAddress() {
 	return storedData
 }
 
-async function getGetAmountsOut(msgValueInput,swapPath) {  
-	const storedData = await contractUniswapV2Router02.getAmountsOut(msgValueInput,swapPath)
+async function getApprovalERC20() {  
+	const storedData = await contractIERC20.allowance(signer.address,addressUniswapV2Router02)
 	return storedData
 }
 
-async function UniswapV2RouterSwapExactETHForTokens() {
+async function UniswapV2RouterAddLiquidityETH() {
 
 	const connectedNetworkObject = await provider.getNetwork();
 	const chainIdConnected = connectedNetworkObject.chainId;
@@ -46,27 +46,21 @@ async function UniswapV2RouterSwapExactETHForTokens() {
 	let factoryAddress = await getFactoryAddress()
 	console.log("factoryAddress: " + factoryAddress)
 
-	const tokenIn = wrappedTokenAddress;
-	const tokenOut = tokenERC20Address;
-	const swapPath = [tokenIn,tokenOut];
-	console.log("swapPath: ", swapPath);
-
-	let msgValueInput = 1041;
-	let getAmountsOutReturnArray = await getGetAmountsOut(msgValueInput,swapPath);
-	let amountOut = getAmountsOutReturnArray[1];
-	console.log("amountIn getAmountsOutReturnArray[0]: "  + getAmountsOutReturnArray[0])
-	console.log("amountOut getAmountsOutReturnArray[1]: " + amountOut)
+	let approvalERC20 = await getApprovalERC20()
+	console.log("approvalERC20: " + approvalERC20)
 
 	// Helps with Shardeum Betanet 1.X nonce issue.
 	const txCount = await provider.getTransactionCount(signer.address); 
 
-	const txSigned = await contractUniswapV2Router02.swapExactETHForTokens(
-		amountOut,
-		swapPath,
+	const txSigned = await contractUniswapV2Router02.addLiquidityETH(
+		tokenERC20Address,
+		2000,
+		1000,
+		1000,
 		signer.address,
 		"115792089237316195423570985008687907853269984665640564039457584007913129639935",
 		{
-			value: msgValueInput,
+			value: 2000,
 			nonce: txCount
 		}
 	); 
