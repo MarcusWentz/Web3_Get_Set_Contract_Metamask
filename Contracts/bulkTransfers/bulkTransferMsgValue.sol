7504da834bc1c7a31d2fb06c453933a2517d5665
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2023-05-14 15:48:06 -0400

    Update bulkTransferMsgValue.sol

--- Contracts/bulkTransfers/bulkTransferMsgValue.sol
+++ Contracts/bulkTransfers/bulkTransferMsgValue.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity 0.8.18;
+
+error msgValueIncorrect();
+error etherNotSent();
 
 contract bulkSender {
 
     function bulkSend(address[] memory myArray) public payable {
+        if(msg.value != (myArray.length * (1 ether)) ) revert msgValueIncorrect();
         for (uint256 i = 0; i < myArray.length;) {
-            payable(myArray[i]).transfer(1 ether);
+            (bool sent, ) = myArray[i].call{value: 1 ether}("");
+            if(sent == false) revert etherNotSent();
             unchecked { i += 1; }
         }
     }
