40209180f0ead832bc6fe4c052984c9a3e94312f
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2023-05-18 13:52:57 -0400

    Update Store.sol

--- Contracts/Store.sol
+++ Contracts/Store.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity 0.8.18;
 
 contract SimpleStorage {
-    uint public storedData; //Do not set 0 manually it wastes gas!
+    uint256 public storedData; //Do not set 0 manually it wastes gas!
 
     event setEvent();
 
-    function set(uint x) public {
+    function set(uint256 x) public {
         storedData = x;
         emit setEvent();
     }
