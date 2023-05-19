73940cb1af60b3636d88be8fa43f23db92873c57
marcuswentz <markzcollege404@gmail.com>
2023-05-19 10:51:02 -0400

    Update example Sepolia

--- Scripts/javascript/web3localTxAndSub.js
+++ Scripts/javascript/web3localTxAndSub.js
@@ -1,13 +1,13 @@
 const ethers = require("ethers")
 
-const rpcURL = process.env.goerliWebSocketSecureEventsInfuraAPIKey // Your RPC URL goes here
+const rpcURL = process.env.sepoliaInfuraWSS // Your RPC URL goes here
 
 // const rpcURL = "http://localhost:8545"// Your RPC URL goes here
 
 const provider = new ethers.providers.WebSocketProvider(rpcURL)
 const signer = new ethers.Wallet(Buffer.from(process.env.devTestnetPrivateKey, 'hex'), provider);
 
-const contractAddress = '0xdbaA7dfBd9125B7a43457D979B1f8a1Bd8687f37'
+const contractAddress = '0xBBE97Afb978E19033e0BDa692E6034F5b3B91312'
 const contractABI = [{"anonymous":false,"inputs":[],"name":"setEvent","type":"event"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
 
 // const contractDeployed = new web3.eth.Contract(contractABI, contractAddress)
