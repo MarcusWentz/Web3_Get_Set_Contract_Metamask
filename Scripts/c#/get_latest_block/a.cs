895440f531bfd1a37b410dd7ccb006bea82d41cc
marcuswentz <markzcollege404@gmail.com>
2023-05-21 18:34:44 -0400

    C# Websocket blocks

--- Scripts/c#/get_latest_block/a.cs
+++ Scripts/c#/get_latest_block/a.cs
@@ -2,17 +2,13 @@
 using System.Threading.Tasks; //Run with: dotnet run
 using Nethereum.Web3; // Install with: dotnet add package Nethereum.Web3
 
-namespace nethereumapp //Guide: https://www.quicknode.com/guides/ethereum-development/getting-started/connecting-to-blockchains/how-to-connect-to-ethereum-using-net-nethereum/#connecting-with-ethereum
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace nethereumapp { //Guide: https://www.quicknode.com/guides/ethereum-development/getting-started/connecting-to-blockchains/how-to-connect-to-ethereum-using-net-nethereum/#connecting-with-ethereum
+    class Program {
+        static void Main(string[] args) {
             GetBlockNumber().Wait();
         }
 
-        static async Task GetBlockNumber()
-        {
+        static async Task GetBlockNumber() {
             string rpcSepoliaHttps = Environment.GetEnvironmentVariable("sepoliaInfuraHttps") ?? throw new InvalidOperationException();
             var web3 = new Web3(rpcSepoliaHttps);
             var latestBlockNumber = await web3.Eth.Blocks.GetBlockNumber.SendRequestAsync();
---
+++ Scripts/c#/websocket_subscribe_blocks/a.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nethereum.JsonRpc.WebSocketClient;
+using Nethereum.Web3;
+using Nethereum.Web3.Accounts.Managed;
+
+namespace nethereumapp {
+    class Program {
+        static void Main(string[] args){
+            GetBlockNumberViaWebSocketAsync().Wait();
+        }
+
+        public static async Task GetBlockNumberViaWebSocketAsync(){
+            string rpcSepoliaWss = Environment.GetEnvironmentVariable("sepoliaInfuraWSS") ?? throw new InvalidOperationException();
+            using(var clientws = new WebSocketClient(rpcSepoliaWss))
+            { 
+                var web3ws = new Web3(clientws);
+                var blockNumber = await web3ws.Eth.Blocks.GetBlockNumber.SendRequestAsync(); //task cancelled exception
+                Console.WriteLine("Block Number: " + blockNumber);
+            }
+        }
+    }
+}
