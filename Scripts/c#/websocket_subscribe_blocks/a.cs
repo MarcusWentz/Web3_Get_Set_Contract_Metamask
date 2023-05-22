dbdf18ea5984fbb449cee0ee93fb1f2576f0c602
marcuswentz <markzcollege404@gmail.com>
2023-05-21 20:12:51 -0400

    C# Fixed install steps comments websocket new blocks listener

--- Scripts/c#/websocket_subscribe_blocks/a.cs
+++ Scripts/c#/websocket_subscribe_blocks/a.cs
@@ -1,23 +1,75 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Nethereum.JsonRpc.WebSocketClient;
-using Nethereum.Web3;
-using Nethereum.Web3.Accounts.Managed;
+using System.Reactive.Linq; // dotnet add package System.Reactive
+using Nethereum.JsonRpc.WebSocketStreamingClient;
+using Nethereum.RPC.Reactive.Eth.Subscriptions; // dotnet add package Nethereum.RPC.Reactive
+using Newtonsoft.Json;
 
-namespace nethereumapp {
-    class Program {
+namespace Nethereum.WebSocketsStreamingTest
+{
+    public class BlockHeader
+    {
         static void Main(string[] args){
-            GetBlockNumberViaWebSocketAsync().Wait();
+            NewBlockHeader_With_Observable_Subscription().Wait();
         }
 
-        public static async Task GetBlockNumberViaWebSocketAsync(){
+        public static async Task NewBlockHeader_With_Observable_Subscription()
+        {
             string rpcSepoliaWss = Environment.GetEnvironmentVariable("sepoliaInfuraWSS") ?? throw new InvalidOperationException();
-            using(var clientws = new WebSocketClient(rpcSepoliaWss))
-            { 
-                var web3ws = new Web3(clientws);
-                var blockNumber = await web3ws.Eth.Blocks.GetBlockNumber.SendRequestAsync(); //task cancelled exception
-                Console.WriteLine("Block Number: " + blockNumber);
+
+            using(var client = new StreamingWebSocketClient(rpcSepoliaWss))
+            {
+                // create the subscription
+                // (it won't start receiving data until Subscribe is called)
+                var subscription = new EthNewBlockHeadersObservableSubscription(client);
+
+                // attach a handler for when the subscription is first created (optional)
+                // this will occur once after Subscribe has been called
+                subscription.GetSubscribeResponseAsObservable().Subscribe(subscriptionId =>
+                    Console.WriteLine("Block Header subscription Id: " + subscriptionId));
+
+                DateTime? lastBlockNotification = null;
+                double secondsSinceLastBlock = 0;
+
+                // attach a handler for each block
+                // put your logic here
+                subscription.GetSubscriptionDataResponsesAsObservable().Subscribe(block => 
+                {
+                    secondsSinceLastBlock = (lastBlockNotification == null) ? 0 : (int)DateTime.Now.Subtract(lastBlockNotification.Value).TotalSeconds;
+                    lastBlockNotification = DateTime.Now;
+                    var utcTimestamp = DateTimeOffset.FromUnixTimeSeconds((long)block.Timestamp.Value);
+                    Console.WriteLine($"New Block. Number: {block.Number.Value}, Timestamp UTC: {JsonConvert.SerializeObject(utcTimestamp)}, Seconds since last block received: {secondsSinceLastBlock} ");
+                });
+
+                bool subscribed = true;
+
+                // handle unsubscription
+                // optional - but may be important depending on your use case
+                subscription.GetUnsubscribeResponseAsObservable().Subscribe(response =>
+                { 
+                    subscribed = false;
+                    Console.WriteLine("Block Header unsubscribe result: " + response);
+                });
+
+                // open the websocket connection
+                await client.StartAsync();
+
+                // start the subscription
+                // this will only block long enough to register the subscription with the client
+                // once running - it won't block whilst waiting for blocks
+                // blocks will be delivered to our handler on another thread
+                await subscription.SubscribeAsync();
+
+                // run for a minute before unsubscribing
+                await Task.Delay(TimeSpan.FromMinutes(1)); 
+
+                // unsubscribe
+                await subscription.UnsubscribeAsync();
+
+                //allow time to unsubscribe
+                while (subscribed) await Task.Delay(TimeSpan.FromSeconds(1));
             }
         }
     }
 }
+
