78f5b1084b467698ad991432c58a4ca509f984ed
marcuswentz <markzcollege404@gmail.com>
2023-05-23 22:53:21 -0400

    Java unix time

--- Scripts/java/src/main/java/com/example/read.java
+++ Scripts/java/src/main/java/com/example/read.java
@@ -1,6 +1,7 @@
 package com.example; 
-// import java.io.IOException;
-// import java.math.BigInteger;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.time.Instant;
 // import org.web3j.protocol.Web3j; //Maven library which goes into pom.xml found here: https://github.com/web3j/web3j#maven
 // import org.web3j.protocol.http.HttpService; 
 
@@ -8,6 +9,8 @@ public class read {
     public static void main(String[] args) { //Click "Run" in VSCodium above this main function.
         String rpcUrlSepoliaHttps = System.getenv("sepoliaInfuraHttps");
         System.out.println("rpcUrlSepoliaHttps: " + rpcUrlSepoliaHttps);
+        long unixTimestamp = Instant.now().getEpochSecond();
+        System.out.println("unixTimestamp: " + unixTimestamp);
 
         // Web3j web3 = Web3j.build(new HttpService(rpcUrlSepoliaHttps)); // Need Java 1.8: sudo update-alternatives --set java /usr/lib/jvm/jdk1.8.0_version/bin/java
 
