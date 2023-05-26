0f1e9463aa4142a516fad219cc0bdbdd43496b16
marcuswentz <markzcollege404@gmail.com>
2023-05-25 23:55:54 -0400

    Java update comments

--- Scripts/java/src/main/java/com/example/Main.java
+++ Scripts/java/src/main/java/com/example/Main.java
@@ -1,4 +1,4 @@
-import java.io.IOException; //Run in IntelliJ Java IDE
+import java.io.IOException; //Run in IntelliJ Java IDE with Maven support libraries 
 import java.math.BigInteger;
 import java.time.Instant;   //Upgrade Maven: https://www.digitalocean.com/community/tutorials/install-maven-linux-ubuntu
 import org.web3j.protocol.Web3j; //Maven library which goes into pom.xml found here: https://github.com/web3j/web3j#maven
@@ -6,13 +6,13 @@ import org.web3j.protocol.http.HttpService;
 
 public class Main {
 
-    public static void main(String[] args) { //Click "Run" in VSCodium above this main function.
+    public static void main(String[] args) { 
         String rpcUrlSepoliaHttps = System.getenv("sepoliaInfuraHttps"); // Set in Modify Run Configuration: https://www.youtube.com/watch?v=oYfd9pDXip8
         System.out.println("rpcUrlSepoliaHttps: " + rpcUrlSepoliaHttps);
         long unixTimestamp = Instant.now().getEpochSecond();
         System.out.println("unixTimestamp: " + unixTimestamp);
 
-        Web3j web3 = Web3j.build(new HttpService(rpcUrlSepoliaHttps)); // Need Java 1.8: sudo update-alternatives --set java /usr/lib/jvm/jdk1.8.0_version/bin/java
+        Web3j web3 = Web3j.build(new HttpService(rpcUrlSepoliaHttps)); 
         System.out.println("web3: " + web3);
 
          try {
