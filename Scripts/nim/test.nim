4f6f468b9f7f44cff90b5dfa9b48424a04d06b45
MarcusWentz <markzcollege404@gmail.com>
2023-06-03 21:04:44 -0400

    Nim simplify signer contract connection

--- Scripts/nim/test.nim
+++ Scripts/nim/test.nim
@@ -12,33 +12,39 @@ proc testFunction(): Future[int] {.async.} =
     #await sleepAsync(100.milliseconds)
     let rpcURL = getEnv("sepoliaInfuraWSS");
     let privateKey = getEnv("devTestnetPrivateKey");
+  
     let provider = JsonRpcProvider.new(rpcURL)
     let signer = Wallet.new(privateKey, provider)
-    echo provider.type()
-    let accounts = await provider.listAccounts()
-    echo accounts.type()
+    
+    # echo provider.type()
+    # let accounts = await provider.listAccounts()
+    # echo accounts.type()
+
     let chainId =  await provider.getChainId()
     echo chainId
 
     type contractSimpleStorage = ref object of Contract
     proc storedData(token: contractSimpleStorage): UInt256 {.contract, view.} # Read function
     proc set(token: contractSimpleStorage, x: UInt256) {.contract.} # Write function
+    type setEvent = object of Event # Event defined
 
     let contractAddress = Address.init("0xBBE97Afb978E19033e0BDa692E6034F5b3B91312")
     echo contractAddress.get()
    
-    let contractInstance = contractSimpleStorage.new(contractAddress.get(), provider)
-    # let contractInstance = contractSimpleStorage.new(contractAddress.get(), signer)
+    # let contractInstance = contractSimpleStorage.new(contractAddress.get(), provider)
+    let contractInstance = contractSimpleStorage.new(contractAddress.get(), signer)
 
     let storedDataValue = await contractInstance.storedData()
     echo storedDataValue
 
-    let contractInstanceWithSigner = contractInstance.connect(signer)
-
-    # let transaction = await contractInstanceWithSigner.set(42.u256)
     let timeNowRoundDown = int(floor(epochTime()))
     echo timeNowRoundDown
-    await contractInstanceWithSigner.set(timeNowRoundDown.u256)
+    await contractInstance.set(timeNowRoundDown.u256)
+
+    # proc handleTransfer(setEvent: setEvent) =
+    #     echo "received transfer: ", setEvent
+
+    #     let subscription = await token.subscribe(Transfer, handleTransfer)
 
     return 1
 
