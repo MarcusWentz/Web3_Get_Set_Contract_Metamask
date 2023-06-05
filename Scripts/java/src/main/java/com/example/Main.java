ef152922bb002356902f032872d5d4d81dd6b0c2
MarcusWentz <markzcollege404@gmail.com>
2023-06-04 23:09:35 -0400

    Java event subscribe working

--- Scripts/java/src/main/java/com/example/Main.java
+++ Scripts/java/src/main/java/com/example/Main.java
@@ -2,14 +2,17 @@ import java.io.IOException; //Run in IntelliJ Java IDE with Maven support librar
 import java.math.BigInteger;
 import java.time.Instant;   //Upgrade Maven: https://www.digitalocean.com/community/tutorials/install-maven-linux-ubuntu
 
-import org.example.store.Store;
-import org.web3j.crypto.Credentials; //IntelliJ Import: File > Project Structure > Libraries > + > From Maven
-import org.web3j.crypto.WalletUtils; //org.web3.crypto
+//IntelliJ Import: File > Project Structure > Libraries > + > From Maven
+import org.web3j.crypto.Credentials;  //org.web3.crypto
 import org.web3j.protocol.Web3j; //org.web3.core
+import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.response.EthChainId;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.protocol.websocket.WebSocketService;
 import org.web3j.tx.gas.DefaultGasProvider;
+import org.web3j.protocol.core.methods.request.EthFilter;
+
 import org.example.store.Store;
 
 public class Main {
@@ -56,6 +59,30 @@ public class Main {
 
         TransactionReceipt tx = contract.set(unixTimestampBigInt).send();
         System.out.println("tx : " + tx);
+
+        //Setup smart contract events: https://stackoverflow.com/questions/67641569/web3j-getting-last-events-from-a-smart-contract
+        String rpcUrlSepoliaWss = System.getenv("sepoliaInfuraWSS"); // Set in Modify Run Configuration: https://www.youtube.com/watch?v=oYfd9pDXip8
+
+        WebSocketService wss = new WebSocketService(rpcUrlSepoliaWss, false);
+        try {
+            wss.connect();
+        } catch (Exception e) {
+            System.out.println("Error while connecting to WSS service: " + e);
+            throw e;
+        }
+
+        Web3j web3jWebsocket = Web3j.build(wss);
+
+        EthFilter filter = new EthFilter(DefaultBlockParameterName.LATEST, DefaultBlockParameterName.LATEST, contractAddress);
+
+        web3jWebsocket.ethLogFlowable(filter).subscribe(event -> {
+            System.out.println("Event received");
+            System.out.println(event);
+            BigInteger storedDataValueEvent = contract.storedData().send();
+            System.out.println("storedDataValueEvent : " + storedDataValueEvent);
+        }, error -> {
+            System.out.println("Error: " + error);
+        });
 
     }
 }
