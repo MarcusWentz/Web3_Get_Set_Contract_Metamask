2786a7a1f0d5564e8fbc76a7d7d13660b6b5cc71
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2023-06-30 23:31:23 -0400

    Update fe_contract_deploy.js

--- Scripts/javascript/fe_contract_deploy.js
+++ Scripts/javascript/fe_contract_deploy.js
@@ -8,9 +8,9 @@ deploy_fe_contract()
 
 async function deploy_fe_contract(){
 
-    const contractBytecode = "0x"+"604f8038039060405180156046575b82810160405239600180546001600160601b03163360601b6001600160601b0319161790554260009081556001908190604e90396000f35b506060600e56fe00";
-    const contractAbi = [{"type":"constructor","name":"__init__","inputs":[],"outputs":[],"stateMutability":"payable"},{"type":"event","name":"error_same_value","inputs":[],"anonymous":false},{"type":"event","name":"event_value_update","inputs":[{"name":"value","type":"uint256","indexed":false}],"anonymous":false},{"type":"event","name":"Context","inputs":[],"anonymous":false}];
-    console.log("contractAbi " + contractAbi)
+    const contractBytecode = "0x"+"610098803803906040518015610049575b82810160405239600180546001600160601b03163360601b6001600160601b031916179055426000908155604690819061005290396000f35b50606061001056fe60003560e01c8063858767ec14602d57638da5cb5b14601a57005b602060015460601c60286035565b908152f35b602060005460285b604051908115604057565b6060915056";
+    const contractAbi = [{"type":"constructor","name":"__init__","inputs":[],"outputs":[],"stateMutability":"payable"},{"type":"function","name":"storage_slot_0","inputs":[],"outputs":[{"name":"","type":"uint256"}],"stateMutability":"view"},{"type":"function","name":"owner","inputs":[],"outputs":[{"name":"","type":"address"}],"stateMutability":"view"},{"type":"event","name":"error_same_value","inputs":[],"anonymous":false},{"type":"event","name":"event_value_update","inputs":[{"name":"value","type":"uint256","indexed":false}],"anonymous":false},{"type":"event","name":"Context","inputs":[],"anonymous":false}];
+    console.log("contractAbi " + contractAbi);
     // const constructorEncodedData = contractAbi.ethers.encodeDeploy(); //No constructor arguments.
     // const txData = hexlify(concat([contractBytecode, constructorEncodedData]));
     const txData = contractBytecode;
