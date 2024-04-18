a2c5198286ef4094e0354ea091b1bfebbc29379b
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2024-04-17 19:23:57 -0700

    Create TransientStorage.sol

---
+++ Contracts/TransientStorage.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+// contract ReentrancyGuardTransientTest {
+//     uint256 public loopCounter;
+//     bytes32 constant SLOT = 0; 
+
+//     modifier lock() {
+//         assembly {
+//             if tload(SLOT) { revert(0, 0) }
+//             tstore(SLOT, 1)
+//         }
+//         _;
+//         assembly {
+//             tstore(SLOT, 0)
+//         }
+//     }
+
+//     function callOnce(uint256 loopValue) internal lock {
+//         loopCounter = loopValue;   
+//     }
+
+//     function loopCaller(uint256 loopCount) public{
+//         for (uint256 i = 0; i < loopCount;) {
+//             callOnce(i);
+//             unchecked { i += 1; }
+//         }
+//     }
+//
+// }
+
+
+contract ReentrancyGuardTransient {
+
+    bytes32 constant SLOT = 0; 
+
+    modifier lock() {
+        assembly {
+            if tload(SLOT) { revert(0, 0) }
+            tstore(SLOT, 1)
+        }
+        _;
+        assembly {
+            tstore(SLOT, 0)
+        }
+    }
+    
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public lock {
+        uint256 bal = balances[msg.sender];
+        require(bal > 0);
+
+        (bool sent,) = msg.sender.call{value: bal}("");
+        require(sent, "Failed to send Ether");
+
+        balances[msg.sender] = 0;
+    }
+
+}
+
+contract Attack {
+    ReentrancyGuardTransient public etherStore;
+    uint256 public constant AMOUNT = 1 ether;
+
+    constructor(address _etherStoreAddress) {
+        etherStore = ReentrancyGuardTransient(_etherStoreAddress);
+    }
+
+    // Fallback is called when EtherStore sends Ether to this contract.
+    fallback() external payable {
+        if (address(etherStore).balance >= AMOUNT) {
+            etherStore.withdraw();
+        }
+    }
+
+    function attack() external payable {
+        require(msg.value >= AMOUNT);
+        etherStore.deposit{value: AMOUNT}();
+        etherStore.withdraw();
+    }
+
+}
