be545c0facb6ec407f769051e2f90df9ec3172b0
MarcusWentz <markzcollege404@gmail.com>
2024-07-10 16:34:03 -0400

    Javascript Base Sepolia

--- Scripts/javascript/readWriteSubscribe.js
+++ Scripts/javascript/readWriteSubscribe.js
@@ -1,6 +1,7 @@
 const ethers = require("ethers") // npm i ethers@5.7.2 https://github.com/smartcontractkit/full-blockchain-solidity-course-js/discussions/5139#discussioncomment-5444517
 
-const rpcURL = process.env.sepoliaInfuraWSS // Your RPC URL goes here
+const rpcURL = process.env.baseSepoliaWSS // Your RPC URL goes here
+// const rpcURL = process.env.sepoliaInfuraWSS // Your RPC URL goes here
 
 // const rpcURL = "http://localhost:8545"// Your RPC URL goes here
 
@@ -10,15 +11,16 @@ const rpcURL = process.env.sepoliaInfuraWSS // Your RPC URL goes here
 const provider = new ethers.providers.WebSocketProvider(rpcURL)
 const signer = new ethers.Wallet(Buffer.from(process.env.devTestnetPrivateKey, 'hex'), provider);
 
-const contractAddress = '0xBBE97Afb978E19033e0BDa692E6034F5b3B91312'
+const contractAddress = 'eD62F27e9e886A27510Dc491F5530996719cEd3d'
 const contractABI = [{"anonymous":false,"inputs":[],"name":"setEvent","type":"event"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
 
 // const contractDeployed = new web3.eth.Contract(contractABI, contractAddress)
 
 const contractDeployed = new ethers.Contract(contractAddress, contractABI, signer);
 
+let base_sepolia_chain_id = 84532;
+
 createAndSendTx()
-getStoredData()
 
 async function getStoredData() {  
   const storedData = await contractDeployed.storedData()
@@ -31,8 +33,17 @@ async function createAndSendTx() {
   const chainIdConnected = connectedNetworkObject.chainId;
   console.log("chainIdConnected: "+ chainIdConnected)
 
+  if(chainIdConnected != base_sepolia_chain_id){
+    console.log("RPC endpoint not connected to Base Sepolia (chainId: " + base_sepolia_chain_id + ").");
+    console.log("Switch to Base Sepolia then try again.");
+    return;
+  }
+
+  await getStoredData();
+
   const unixTime = Date.now();
 
+  console.log("CURRENT UNIX TIME: " + unixTime);
 
   //Simple contract transaction.
   const txSigned = await contractDeployed.set(unixTime); //Will compute the gas limit opcodes automatically and get the oracle gas price per gas unit.
@@ -63,13 +74,13 @@ async function createAndSendTx() {
   //   data: txData
   // });
 
-  console.log(txSigned)
+  console.log("Tx submitted: " + txSigned.hash)
 
 }
 
 contractDeployed.on("setEvent", (eventDetected) => {
 
-  console.log("EVENT DETECTED! EVENT LOG: ")
+  console.log("EVENT DETECTED!")
 
   console.log(eventDetected)
 
