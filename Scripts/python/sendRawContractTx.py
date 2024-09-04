1ac5c76e8f88b19371cc8e3df60bc1e3210c2ca7
MarcusWentz <markzcollege404@gmail.com>
2024-09-04 02:03:31 -0400

    sendRawContractTx

--- Scripts/python/sendRawContractTx.py
+++ Scripts/python/sendRawContractTx.py
@@ -1,6 +1,7 @@
 from web3 import Web3
 import json
 import os
+import time
 
 infura_base_sepolia_testnet_url_API = str(os.environ['baseSepoliaHTTPS']);
 web3 = Web3(Web3.HTTPProvider(infura_base_sepolia_testnet_url_API))
@@ -31,15 +32,23 @@ balance = web3.eth.get_balance(userWallet)
 print("Balance [Base Sepolia ether]" )
 print(web3.from_wei(balance, "ether") )
 
+Contract_At_Address= web3.to_checksum_address("0xeD62F27e9e886A27510Dc491F5530996719cEd3d");
+abi_Contract = json.loads('[{"anonymous":false,"inputs":[],"name":"setEvent","type":"event"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]');
+contract_Call = web3.eth.contract(address = Contract_At_Address , abi = abi_Contract);
+print(contract_Call.functions.storedData().call());
+
+currentUnixTime = round(time.time())
+print(round(currentUnixTime))
+
 # Raw tx
 
 tx = {
     'chainId': web3.eth.chain_id,
     'nonce':  web3.eth.get_transaction_count(userWallet),
-    'to': userWallet, 
-    'gas': 21000, 
-    'gasPrice': web3.eth.gas_price, # https://etherscan.io/gastracker
-    'value': 1
+    'to': Contract_At_Address, 
+    'data': contract_Call.encodeABI(fn_name='set', args=[currentUnixTime]),
+    'gas': 30000, 
+    'gasPrice': web3.eth.gas_price # https://etherscan.io/gastracker
 }
 
 signed_tx = web3.eth.account.sign_transaction(tx, devTestnetPrivateKey)
