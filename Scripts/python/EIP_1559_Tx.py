209df18060941d230856b0671709cea65378a7f6
MarcusWentz <markzcollege404@gmail.com>
2024-09-04 02:50:10 -0400

    Python fix EIP-1559 tx

--- Scripts/python/EIP_1559_Tx.py
+++ Scripts/python/EIP_1559_Tx.py
@@ -2,8 +2,11 @@ from web3 import Web3
 import json
 import os
 
-infura_goerli_testnet_url_API = str(os.environ['goerliHTTPS_InfuraAPIKey']);
-web3 = Web3(Web3.HTTPProvider(infura_goerli_testnet_url_API))
+infura_base_sepolia_testnet_url_API = str(os.environ['baseSepoliaHTTPS']);
+web3 = Web3(Web3.HTTPProvider(infura_base_sepolia_testnet_url_API))
+
+# goerli_testnet_url_API = str(os.environ["http://localhost:8545"]);
+# web3 = Web3(Web3.HTTPProvider(goerli_testnet_url_API))
 
 devTestnetPrivateKey = str(os.environ['devTestnetPrivateKey']);
 
@@ -11,34 +14,34 @@ userWallet = (web3.eth.account.from_key(devTestnetPrivateKey)).address
 print(userWallet)
 
 print("Connected to Web3? ")
-print(web3.isConnected())
-
-# Read information from the blockchain.
-print("Current block? ")
-print(web3.eth.blockNumber)
+print(web3.is_connected())
 
 print("Chain ID? ")
 print(web3.eth.chain_id)
 
-balance = web3.eth.getBalance(userWallet)
-print("Balance [Goerli ether]" )
-print(web3.fromWei(balance, "ether") )
+# Read information from the blockchain.
+print("Current block? ")
+print(web3.eth.block_number)
 
-Contract_At_Address= web3.toChecksumAddress("0xdbaA7dfBd9125B7a43457D979B1f8a1Bd8687f37");
-abi_Contract = json.loads('[{"anonymous":false,"inputs":[],"name":"setEvent","type":"event"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]');
-contract_Call = web3.eth.contract(address = Contract_At_Address , abi = abi_Contract);
-print(contract_Call.functions.storedData().call());
+# Read information from the blockchain.
+print("Gas price? ")
+print(web3.eth.gas_price)
 
-EIP_1559_tx = {
-    'nonce':  web3.eth.getTransactionCount(userWallet)       ,
-    'to': Contract_At_Address, #WORKS WITH REGULAR WALLETS BUT CANNOT SEND TO CONTRACT FOR SOME REASON?
-    'gas': 2000000, #GAS LIMIT. REMOVED FIXED GAS PRICE. NOW DYNAMIC.
-    # 'gasPrice': web3.toWei('50', 'gwei'), # https://etherscan.io/gastracker
-    'maxFeePerGas': web3.toWei(30, 'gwei'),
-    'maxPriorityFeePerGas': web3.toWei(20, 'gwei'),
-    'chainId' : web3.eth.chain_id,
-    'data' : contract_Call.encodeABI(fn_name='set', args=[23131])
+balance = web3.eth.get_balance(userWallet)
+print("Balance [Base Sepolia ether]" )
+print(web3.from_wei(balance, "ether") )
+
+# Raw tx
+
+tx = {
+    'chainId': web3.eth.chain_id,
+    'nonce':  web3.eth.get_transaction_count(userWallet),
+    'to': userWallet, 
+    'gas': 21000, 
+    'maxFeePerGas': web3.to_wei(30, 'gwei'),
+    'maxPriorityFeePerGas': web3.to_wei(20, 'gwei'),
+    'value': 1
 }
 
-signed_tx = web3.eth.account.signTransaction(EIP_1559_tx, devTestnetPrivateKey)
-print(web3.toHex(web3.eth.sendRawTransaction(signed_tx.rawTransaction)))
+signed_tx = web3.eth.account.sign_transaction(tx, devTestnetPrivateKey)
+print(web3.to_hex(web3.eth.send_raw_transaction(signed_tx.raw_transaction)))
