da2525c0582cdfddffb042e798f5bc892bef80c0
MarcusWentz <52706599+MarcusWentz@users.noreply.github.com>
2024-09-27 14:26:42 -0400

    pbrMathSquareRootTesting unwrap values to uint256

--- Contracts/pbrMathSquareRootTesting.sol
+++ Contracts/pbrMathSquareRootTesting.sol
@@ -2,19 +2,29 @@
 pragma solidity 0.8.26;
 
 // Unsigned
-import { UD60x18, ud } from "@prb/math/src/UD60x18.sol"; 
+import { UD60x18 } from "@prb/math/src/UD60x18.sol"; 
 // // Signed
-// import { SD59x18, sd } from "@prb/math/src/SD59x18.sol";
+// import { SD59x18 } from "@prb/math/src/SD59x18.sol";
 
 contract pbrMathSquareRootTesting {
 
   /// @notice Calculates the square root for the given signed number.
-  function unsignedSquareRootTestSmallValue() external pure returns (UD60x18 result) {
+  function unsignedSquareRootTestSmallValueReturnRawValue() external pure returns (UD60x18 result) {
     // 1 wei in uint256 is actually 1 ether in UD60x18 fixed point types.
-    UD60x18 x = ud(1);
+    UD60x18 x = UD60x18.wrap(1);
     // Returns 1000000000, since: 
     // 1 ether = 10**18 = (10**9)**2
     result = x.sqrt(); 
+  }
+
+  /// @notice Calculates the square root for the given signed number.
+  function unsignedSquareRootTestReturnUint256() external pure returns (uint256 result) {
+    // 1 wei in uint256 is actually 1 ether in UD60x18 fixed point types.
+    UD60x18 x = UD60x18.wrap(1);
+    // Returns 1000000000, since: 
+    // 1 ether = 10**18 = (10**9)**2
+    UD60x18 sqrtRawValue = x.sqrt(); 
+    result = sqrtRawValue.unwrap();
   }
 
   /// @notice Calculates the square root for the given signed number.
