51550e87033be852c8d7f17c0e63a977972212a1
MarcusWentz <52706599+MarcusWentz@users.noreply.github.com>
2024-10-21 20:41:15 -0700

    Create TransientStorageVaraibleType.sol

---
+++ Contracts/TransientStorageVaraibleType.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+contract TransientStorageVaraibleType {
+    bool internal transient IS_UNLOCKED_SLOT;
+
+    function unlock() internal {
+        IS_UNLOCKED_SLOT = true;
+    }
+
+    function lock() internal {
+        IS_UNLOCKED_SLOT = false;
+    }
+
+    function isUnlocked() internal view returns (bool unlocked) {
+        unlocked = IS_UNLOCKED_SLOT;
+    }
+}
