7ee290a7c29ed88af5e357ccaa2c3968acf1cb58
marcuswentz <markzcollege404@gmail.com>
2025-02-21 19:43:54 -0500

    Test functions

--- Scripts/rust/fluent/prb_math_return_test/src/lib.rs
+++ Scripts/rust/fluent/prb_math_return_test/src/lib.rs
@@ -1,9 +1,8 @@
 #![cfg_attr(target_arch = "wasm32", no_std)]
 extern crate alloc;
 
-
 // float64 and float32 methods library for complex math functions :
-// (log,log10,log2,sqrt,exp)
+// (ln,log10,log2,sqrt,exp)
 // since we do not have access to the Rust standard library (std) with setting "no_std" with the wasm32 target file.
 use libm;
 
@@ -26,14 +25,12 @@ struct ROUTER<SDK> {
 }
 
 pub trait RouterAPI {
-    // Make sure type interfaces are defined here or else there will be a compiler error.
-    fn rustString(&self) -> String;
-    fn rustUint256(&self) -> U256;
-    // fn rustInt256(&self) -> I256;
-    fn rustAddress(&self) -> Address;
-    fn rustBytes(&self) -> Bytes;
-    fn rustBytes32(&self) -> B256;
-    fn rustBool(&self) -> bool;
+    // Make sure type interfaces are defined here 
+    fn rustLnUint256(&self) -> U256;
+    fn rustLog10Uint256(&self) -> U256;
+    fn rustLog2Uint256(&self) -> U256;
+    fn rustSqrtUint256(&self) -> U256;
+    fn rustExpUint256(&self) -> U256;
 }
 
 #[router(mode = "solidity")]
@@ -42,25 +39,59 @@ impl<SDK: SharedAPI> RouterAPI for ROUTER<SDK> {
     // ERC-20 with Fluent SDK example:
     // https://github.com/fluentlabs-xyz/fluentbase/blob/devel/examples/erc20/lib.rs
 
-    #[function_id("rustString()")]
-    fn rustString(&self) -> String {
-        let string_test = "Hello".to_string();
-        return string_test;
-    }
-
-    #[function_id("rustUint256()")]
-    fn rustUint256(&self) -> U256 {
+    #[function_id("rustLnUint256()")]
+    fn rustLnUint256(&self) -> U256 {
 
         // // f64 value types have methods for more complicated math operations.
         let input: f64 = 100.0;
 
         let ln_result = libm::log(input); // Natural log (ln)
 
+        let uint256_test = U256::from(10);
+        return uint256_test;
+    }
+
+    #[function_id("rustLog10Uint256()")]
+    fn rustLog10Uint256(&self) -> U256 {
+
+        // // f64 value types have methods for more complicated math operations.
+        let input: f64 = 100.0;
+
         let log10_result = libm::log10(input); // Calculates the base-10 logarithm of input
+
+        let uint256_test = U256::from(10);
+        return uint256_test;
+    }
+
+    #[function_id("rustLog2Uint256()")]
+    fn rustLog2Uint256(&self) -> U256 {
+
+        // // f64 value types have methods for more complicated math operations.
+        let input: f64 = 100.0;
 
         let log2_result = libm::log2(input); // Calculates the base-2 logarithm of input
 
+        let uint256_test = U256::from(10);
+        return uint256_test;
+    }
+
+    #[function_id("rustSqrtUint256()")]
+    fn rustSqrtUint256(&self) -> U256 {
+
+        // // f64 value types have methods for more complicated math operations.
+        let input: f64 = 100.0;
+
         let sqrt_result = libm::sqrt(input); // Calculates the square root of input
+
+        let uint256_test = U256::from(10);
+        return uint256_test;
+    }
+
+    #[function_id("rustExpUint256()")]
+    fn rustExpUint256(&self) -> U256 {
+
+        // // f64 value types have methods for more complicated math operations.
+        let input: f64 = 100.0;
 
         let exp_result = libm::exp(input); // Calculates the exp (e^x) of input
 
@@ -68,34 +99,6 @@ impl<SDK: SharedAPI> RouterAPI for ROUTER<SDK> {
         return uint256_test;
     }
 
-    // #[function_id("rustInt256()")]
-    // fn rustInt256(&self) -> I256 {
-    //     return I256::from(-10)
-    // }
-
-    #[function_id("rustAddress()")]
-    fn rustAddress(&self) -> Address {
-        let address_test: Address = address!("d8da6bf26964af9d7eed9e03e53415d37aa96045"); // vitalik.eth 0xd8da6bf26964af9d7eed9e03e53415d37aa96045
-        return address_test;
-    }
-    
-    #[function_id("rustBytes()")]
-    fn rustBytes(&self) -> Bytes {
-        let bytes_test = Bytes::from("FLUENT");
-        return bytes_test;
-    }
-
-    #[function_id("rustBytes32()")]
-    fn rustBytes32(&self) -> B256 {
-        let bytes256_test = b256!("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-        return bytes256_test;
-    }
-
-    #[function_id("rustBool()")]
-    fn rustBool(&self) -> bool {
-        let bool_test = true;
-        return bool_test;
-    }
 
 }
 
